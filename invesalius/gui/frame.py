3b44f50b88e76596f702df8844d7faff16eee3aa
tatiana <tatiana.alchueyr@gmail.com>
2009-06-19 17:03:32 +0000

    FIX: Toolbar icon size under Win and Linux

--- invesalius/gui/frame.py
+++ invesalius/gui/frame.py
@@ -312,7 +312,8 @@ class LayoutToolBar(wx.ToolBar):
     # TODO: what will appear in menubar?
     def __init__(self, parent):
         wx.ToolBar.__init__(self, parent, -1, wx.DefaultPosition, wx.DefaultSize, wx.TB_FLAT|wx.TB_NODIVIDER)
-        self.SetToolBitmapSize(wx.Size(25,25))
+        if sys.platform == 'darwin':
+            self.SetToolBitmapSize(wx.Size(25,25))
         self.parent = parent
         self.__init_items()
         self.__bind_events()
