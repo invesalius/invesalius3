447a269e787d59d949b7403488100aa3ebf57bc6
paulojamorim <paulojamorim@gmail.com>
2009-10-22 02:25:32 +0000

    FIX: Bug in the progress dialog from Load Dicom's, in the Windows XP 64 Bits with wxPython 2.8.10

--- invesalius/gui/dialogs.py
+++ invesalius/gui/dialogs.py
@@ -85,7 +85,12 @@ class ProgressDialog(object):
                 
     def Update(self, value, message):
         if(int(value) != self.maximum):  
-            self.dlg.Update(value,message)
+            try:
+                self.dlg.Update(value,message)
+            #TODO:
+            #Exception in the Windows XP 64 Bits with wxPython 2.8.10
+            except(wx._core.PyAssertionError):
+                pass
             return True
         else:
             return False
