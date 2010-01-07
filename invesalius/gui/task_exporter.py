3829e21ef4ed3fabbd6675a8e11d37e618c74b1d
tatiana <tatiana.alchueyr@gmail.com>
2010-01-07 13:03:34 +0000

    ENH: Hid mask button used to export imagedata (#89)

--- invesalius/gui/task_exporter.py
+++ invesalius/gui/task_exporter.py
@@ -119,15 +119,15 @@ class InnerTaskPanel(wx.Panel):
         link_export_surface.Bind(hl.EVT_HYPERLINK_LEFT,
                               self.OnLinkExportSurface)
 
-        tooltip = wx.ToolTip(_("Export 3D mask (voxels)"))
-        link_export_mask = hl.HyperLinkCtrl(self, -1,_("Export mask..."))
-        link_export_mask.SetUnderlines(False, False, False)
-        link_export_mask.SetColours("BLACK", "BLACK", "BLACK")
-        link_export_mask.SetToolTip(tooltip)
-        link_export_mask.AutoBrowse(False)
-        link_export_mask.UpdateLink()
-        link_export_mask.Bind(hl.EVT_HYPERLINK_LEFT,
-                              self.OnLinkExportMask)
+        #tooltip = wx.ToolTip(_("Export 3D mask (voxels)"))
+        #link_export_mask = hl.HyperLinkCtrl(self, -1,_("Export mask..."))
+        #link_export_mask.SetUnderlines(False, False, False)
+        #link_export_mask.SetColours("BLACK", "BLACK", "BLACK")
+        #link_export_mask.SetToolTip(tooltip)
+        #link_export_mask.AutoBrowse(False)
+        #link_export_mask.UpdateLink()
+        #link_export_mask.Bind(hl.EVT_HYPERLINK_LEFT,
+        #                      self.OnLinkExportMask)
 
 
         #tooltip = wx.ToolTip("Request rapid prototyping services")
@@ -157,19 +157,19 @@ class InnerTaskPanel(wx.Panel):
             BMP_TAKE_PICTURE = wx.Bitmap(\
                                  "../icons/tool_photo_original.png",
                                  wx.BITMAP_TYPE_PNG)
-            BMP_EXPORT_MASK = wx.Bitmap("../icons/mask.png",
-                                        wx.BITMAP_TYPE_PNG)
+            #BMP_EXPORT_MASK = wx.Bitmap("../icons/mask.png",
+            #                            wx.BITMAP_TYPE_PNG)
         else:
             BMP_EXPORT_SURFACE = wx.Bitmap("../icons/surface_export.png",
                                         wx.BITMAP_TYPE_PNG)
             BMP_TAKE_PICTURE = wx.Bitmap("../icons/tool_photo.png",
                                      wx.BITMAP_TYPE_PNG)
-            BMP_EXPORT_MASK = wx.Bitmap("../icons/mask_small.png",
-                                        wx.BITMAP_TYPE_PNG)
+            #BMP_EXPORT_MASK = wx.Bitmap("../icons/mask_small.png",
+            #                            wx.BITMAP_TYPE_PNG)
 
 
-        bmp_list = [BMP_TAKE_PICTURE, BMP_EXPORT_SURFACE,
-                    BMP_EXPORT_MASK]
+        bmp_list = [BMP_TAKE_PICTURE, BMP_EXPORT_SURFACE]#,
+        #            BMP_EXPORT_MASK]
         for bmp in bmp_list:
             bmp.SetWidth(25)
             bmp.SetHeight(25)
@@ -183,9 +183,9 @@ class InnerTaskPanel(wx.Panel):
         button_surface = pbtn.PlateButton(self, BTN_SURFACE, "",
                                                 BMP_EXPORT_SURFACE,
                                               style=button_style)
-        button_mask = pbtn.PlateButton(self, BTN_MASK, "",
-                                        BMP_EXPORT_MASK,
-                                        style=button_style)
+        #button_mask = pbtn.PlateButton(self, BTN_MASK, "",
+        #                                BMP_EXPORT_MASK,
+        #                                style=button_style)
         #button_request_rp = pbtn.PlateButton(self, BTN_REQUEST_RP, "",
         #                                    BMP_IMPORT, style=button_style)
         #button_report = pbtn.PlateButton(self, BTN_REPORT, "",
@@ -199,14 +199,14 @@ class InnerTaskPanel(wx.Panel):
         flag_link = wx.EXPAND|wx.GROW|wx.LEFT|wx.TOP
         flag_button = wx.EXPAND | wx.GROW
 
-        fixed_sizer = wx.FlexGridSizer(rows=3, cols=2, hgap=2, vgap=0)
+        fixed_sizer = wx.FlexGridSizer(rows=2, cols=2, hgap=2, vgap=0)
         fixed_sizer.AddGrowableCol(0, 1)
         fixed_sizer.AddMany([ (link_export_picture, 1, flag_link, 3),
                               (button_picture, 0, flag_button),
                               (link_export_surface, 1, flag_link, 3),
-                              (button_surface, 0, flag_button),#])#,
-                              (link_export_mask, 1, flag_link, 3),
-                              (button_mask, 0, flag_button)])
+                              (button_surface, 0, flag_button),])
+                              #(link_export_mask, 1, flag_link, 3),
+                              #(button_mask, 0, flag_button)])
                               #(link_report, 0, flag_link, 3),
                               #(button_report, 0, flag_button),
                               #(link_request_rp, 1, flag_link, 3),
