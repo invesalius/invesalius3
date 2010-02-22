8c60d1404bbeb4c59a2a609f2951684d3d59eab6
vhosouza <victorhos@hotmail.com>
2010-02-22 17:19:44 +0000

    ENH: Surface camera update on neuronavigation and image spacing correction.

--- invesalius/data/co_registration.py
+++ invesalius/data/co_registration.py
@@ -18,21 +18,10 @@ class Corregister(threading.Thread):
         self.flag = flag
         self._pause_ = 0
         self.start()
-       
-    def __bind_events(self):
-        ps.Publisher().subscribe(self.PixelSpacing, 'Spacing')
         
     def stop(self):
         # Stop neuronavigation
         self._pause_ = 1
-        
-    def PixelSpacing(self):
-        spacing_x = pubsub_evt.data[0]
-        spacing_y = pubsub_evt.data[1]
-        spacing_z = pubsub_evt.data[2]
-        self.correcx = 10.0/spacing_x
-        self.correcy = 10.0/spacing_y
-        self.correcz = 10.0/spacing_z
         
     def Coordinates(self):        
                
@@ -52,16 +41,12 @@ class Corregister(threading.Thread):
         x = 10.0/spacingx
         y = 10.0/spacingy
         z = 10.0/spacingz
-        #coord = (aoflt[0]*10.0, aoflt[1]*10.0, aoflt[2]*10.0)
-        #coord com conversao de cm para pixel (1 pixel = 0.487 mm para c:\dicom2)
-        #print "correcs: ", self.correcx, self.correcy, self.correcz
+
         coord = (aoflt[0]*x, aoflt[1]*y, aoflt[2]*z)
         return coord
        
     def run(self):
-        spacingx, spacingy, spacingz = project.Project().imagedata.GetSpacing()
-        bounds = array(project.Project().imagedata.GetBounds()[::2])       
-        
+             
         while self.flag == True:
             #trck = self.Coordinates()
             for i in range(1, 20):
@@ -69,17 +54,18 @@ class Corregister(threading.Thread):
                 y = sqrt(500.0 - x**2)
                 z = float(i) - 4.0
                 
-                tracker = matrix([[x*spacingx], [y*spacingy], [z*spacingz]])
+                tracker = matrix([[x], [y], [z]])
                 #tracker = matrix([[trck[0]], [trck[1]], [trck[2]]])            
                 img = self.q1 + (self.Minv*self.N)*(tracker - self.q2)
                 
                 coord = [float(img[0]), float(img[1]), float(img[2])]
-                coord_teste = array([float(img[0]), float(img[1]), float(img[2])])
-                camera_coord = coord_teste - bounds
+                coord_cam = (float(img[0]), float(img[1]), float(img[2]))
 
                 #coord = img.tolist()
                 ps.Publisher().sendMessage('Co-registered Points', coord)
-                ps.Publisher().sendMessage('Set Camera Position to Neuronavigate', camera_coord)
+                ps.Publisher().sendMessage('Set camera in volume', coord_cam)
+                ps.Publisher().sendMessage('Render volume viewer')
+                ps.Publisher().sendMessage('Update slice viewer')
                 sleep(1.0)
                 if self._pause_:
                     return
