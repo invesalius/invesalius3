15441ada9881da2d14b57fa1471f642c311c012d
Thiago Franco de Moraes <totonixsame@gmail.com>
2013-09-20 14:55:00 -0300

    Only using compiling with openmp on Linux

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import sys
+
 import numpy
 
-setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [ Extension("invesalius/data/mips", ["invesalius/data/mips.pyx"],
-                              include_dirs =  [numpy.get_include()],
-                              extra_compile_args=['-fopenmp'],
-                              extra_link_args=['-fopenmp'],)]
-     )
+if sys.platform == 'linux2':
+    setup(
+        cmdclass = {'build_ext': build_ext},
+        ext_modules = [ Extension("invesalius.data.mips", ["invesalius/data/mips.pyx"],
+                                  include_dirs =  [numpy.get_include()],
+                                  extra_compile_args=['-fopenmp'],
+                                  extra_link_args=['-fopenmp'],)]
+         )
+else:
+    setup(
+        cmdclass = {'build_ext': build_ext},
+        ext_modules = [ Extension("invesalius.data.mips", ["invesalius/data/mips.pyx"],
+                                  include_dirs =  [numpy.get_include()],)]
+         )
