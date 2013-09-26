b81b6ec07261d402be838441420d62772562a553
Thiago Franco de Moraes <totonixsame@gmail.com>
2013-09-26 15:32:02 -0300

    Added support in setup.py to compile in windows with openmp

--- setup.py
+++ setup.py
@@ -14,6 +14,16 @@ if sys.platform == 'linux2':
                                   extra_compile_args=['-fopenmp'],
                                   extra_link_args=['-fopenmp'],)]
          )
+
+elif sys.platform == 'win32':
+    setup(
+        cmdclass = {'build_ext': build_ext},
+        ext_modules = [ Extension("invesalius.data.mips", ["invesalius/data/mips.pyx"],
+                                  include_dirs =  [numpy.get_include()],
+                                  extra_compile_args=['/openmp'],
+                                 )]
+         )
+
 else:
     setup(
         cmdclass = {'build_ext': build_ext},
