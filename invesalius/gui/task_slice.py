fce40bdfc44fc335b5c963ce82fb496b34f3293e
Thiago Franco de Moraes <totonixsame@gmail.com>
2014-06-24 09:29:12 -0300

    Fixed: it was generating a surface when user cancels the operation

--- invesalius/gui/task_slice.py
+++ invesalius/gui/task_slice.py
@@ -146,6 +146,7 @@ class InnerTaskPanel(wx.Panel):
         overwrite = self.check_box.IsChecked()
         algorithm = 'Default'
         options = {}
+        to_generate = True
         if self.GetMaskSelected() != -1:
             sl = slice_.Slice()
             if sl.current_mask.was_edited:
@@ -153,22 +154,25 @@ class InnerTaskPanel(wx.Panel):
                 if dlgs.ShowModal() == wx.ID_OK:
                     algorithm = dlgs.GetAlgorithmSelected()
                     options = dlgs.GetOptions()
+                else:
+                    to_generate = False
 
                 dlgs.Destroy()
 
-            mask_index = sl.current_mask.index
-            method = {'algorithm': algorithm, 
-                      'options': options}
-            srf_options = {"index": mask_index,
-                           "name": '',
-                           "quality": _('Optimal *'),
-                           "fill": False,
-                           "keep_largest": False,
-                           "overwrite": overwrite}
+            if to_generate:
+                mask_index = sl.current_mask.index
+                method = {'algorithm': algorithm, 
+                          'options': options}
+                srf_options = {"index": mask_index,
+                               "name": '',
+                               "quality": _('Optimal *'),
+                               "fill": False,
+                               "keep_largest": False,
+                               "overwrite": overwrite}
 
-            Publisher.sendMessage('Create surface from index', 
-                                  {'method': method, 'options': srf_options})
-            Publisher.sendMessage('Fold surface task')
+                Publisher.sendMessage('Create surface from index', 
+                                      {'method': method, 'options': srf_options})
+                Publisher.sendMessage('Fold surface task')
 
         else:
             dlg.InexistentMask()
