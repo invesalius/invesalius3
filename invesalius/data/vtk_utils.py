dcdd9e8aee05289fc19bbaf86c9f916048ac8cad
vhosouza <victorhos@hotmail.com>
2015-02-23 15:33:44 -0300

    Revert "Revert "ENH: Update neuronavigator modules""
    This reverts commit 2ecc37998e18c762b82413314fcf75d1491ca1be.

--- invesalius/data/vtk_utils.py
+++ invesalius/data/vtk_utils.py
@@ -142,7 +142,7 @@ class Text(object):
                                                     position[1])
 
     def GetPosition(self, position):
-        self.actor.GetPositionCoordinate().GetValue()
+        self.actor.GetPositionCoordinate().GetValue
 
     def SetJustificationToRight(self):
         self.property.SetJustificationToRight()
@@ -210,7 +210,7 @@ class TextZero(object):
                                                     position[1])
 
     def GetPosition(self, position):
-        self.actor.GetPositionCoordinate().GetValue()
+        self.actor.GetPositionCoordinate().GetValue
 
     def SetJustificationToRight(self):
         self.property.SetJustificationToRight()
