ac56cedc3582840918ca2c8859961e17524b8b7c
Thiago Franco de Moraes <totonixsame@gmail.com>
2016-05-18 13:30:44 -0300

    Surface area (#37)
    * Calculating surface area
    
    * remove prints
    
    * Updating surface info when overwriting surface
    
    * Calculating area when selecting greatest surface, select surface or separate all surfaces
    
    * Copying area when duplicating surface

--- invesalius/data/surface.py
+++ invesalius/data/surface.py
@@ -59,7 +59,8 @@ class Surface():
         self.polydata = ''
         self.colour = ''
         self.transparency = const.SURFACE_TRANSPARENCY
-        self.volume = 0
+        self.volume = 0.0
+        self.area = 0.0
         self.is_shown = 1
         if not name:
             self.name = const.SURFACE_NAME_PATTERN %(self.index+1)
@@ -81,6 +82,7 @@ class Surface():
                    'transparency': self.transparency,
                    'visible': bool(self.is_shown),
                    'volume': self.volume,
+                   'area': self.area,
                   }
         plist_filename = filename + '.plist'
         #plist_filepath = os.path.join(dir_temp, filename + '.plist')
@@ -100,6 +102,10 @@ class Surface():
         self.transparency = sp['transparency']
         self.is_shown = sp['visible']
         self.volume = sp['volume']
+        try:
+            self.area = sp['area']
+        except KeyError:
+            self.area = 0.0
         self.polydata = pu.Import(os.path.join(dirpath, sp['polydata']))
         Surface.general_index = max(Surface.general_index, self.index)
 
@@ -164,7 +170,8 @@ class SurfaceManager():
                                            name = new_name,
                                            colour = original_surface.colour,
                                            transparency = original_surface.transparency,
-                                           volume = original_surface.volume)
+                                           volume = original_surface.volume,
+                                           area = original_surface.area)
 
     def OnRemove(self, pubsub_evt):
         selected_items = pubsub_evt.data
@@ -240,7 +247,7 @@ class SurfaceManager():
 
     def CreateSurfaceFromPolydata(self, polydata, overwrite=False,
                                   name=None, colour=None,
-                                  transparency=None, volume=None):
+                                  transparency=None, volume=None, area=None):
         normals = vtk.vtkPolyDataNormals()
         normals.SetInputData(polydata)
         normals.SetFeatureAngle(80)
@@ -290,7 +297,7 @@ class SurfaceManager():
         session.ChangeProject()
 
         # The following lines have to be here, otherwise all volumes disappear
-        if not volume:
+        if not volume or not area:
             triangle_filter = vtk.vtkTriangleFilter()
             triangle_filter.SetInputData(polydata)
             triangle_filter.Update()
@@ -299,18 +306,22 @@ class SurfaceManager():
             measured_polydata.SetInputConnection(triangle_filter.GetOutputPort())
             measured_polydata.Update()
             volume =  measured_polydata.GetVolume()
+            area =  measured_polydata.GetSurfaceArea()
             surface.volume = volume
+            surface.area = area
             print ">>>>", surface.volume
         else:
             surface.volume = volume
+            surface.area = area
+
         self.last_surface_index = surface.index
 
         Publisher.sendMessage('Load surface actor into viewer', actor)
 
         Publisher.sendMessage('Update surface info in GUI',
-                                        (surface.index, surface.name,
-                                        surface.colour, surface.volume,
-                                        surface.transparency))
+                              (surface.index, surface.name,
+                               surface.colour, surface.volume,
+                               surface.area, surface.transparency))
         return surface.index
 
     def OnCloseProject(self, pubsub_evt):
@@ -329,9 +340,9 @@ class SurfaceManager():
         proj = prj.Project()
         surface = proj.surface_dict[index]
         Publisher.sendMessage('Update surface info in GUI',
-                                    (index, surface.name,
-                                    surface.colour, surface.volume,
-                                    surface.transparency))
+                              (index, surface.name,
+                               surface.colour, surface.volume,
+                               surface.area, surface.transparency))
         self.last_surface_index = index
         if surface.is_shown:
             self.ShowActor(index, True)
@@ -377,8 +388,8 @@ class SurfaceManager():
             # The following lines have to be here, otherwise all volumes disappear
             Publisher.sendMessage('Update surface info in GUI',
                                         (surface.index, surface.name,
-                                        surface.colour, surface.volume,
-                                        surface.transparency))
+                                         surface.colour, surface.volume,
+                                         surface.area, surface.transparency))
             if not surface.is_shown:
                 self.ShowActor(key, False)
 
@@ -727,7 +738,9 @@ class SurfaceManager():
         #  measured_polydata.ReleaseDataFlagOn()
         measured_polydata.SetInputData(to_measure)
         volume =  float(measured_polydata.GetVolume())
+        area =  float(measured_polydata.GetSurfaceArea())
         surface.volume = volume
+        surface.area = area
         self.last_surface_index = surface.index
         del measured_polydata
         del to_measure
@@ -745,6 +758,7 @@ class SurfaceManager():
         Publisher.sendMessage('Update surface info in GUI',
                                     (surface.index, surface.name,
                                     surface.colour, surface.volume,
+                                    surface.area,
                                     surface.transparency))
 
         #When you finalize the progress. The bar is cleaned.
