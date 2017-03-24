eeb5d439e7cef08144a906a07075efd0e067af48
Thiago Franco de Moraes <totonixsame@gmail.com>
2017-03-24 10:03:24 -0300

    Setting Interactor Style to Default when closing a project

--- invesalius/control.py
+++ invesalius/control.py
@@ -355,6 +355,7 @@ class Controller():
 
         Publisher.sendMessage('Hide content panel')
         Publisher.sendMessage('Close project data')
+        Publisher.sendMessage('Set slice interaction style', const.STATE_DEFAULT)
         session = ses.Session()
         session.CloseProject()
 
