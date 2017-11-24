2549368091731acd8e77efa4828f5579c0581a07
Renan <renan_hiroshi@hotmail.com>
2017-11-24 18:05:41 -0200

    Set vol. camera default true
    - TODO: when nav is off, vol is reset to front position

--- invesalius/gui/task_navigator.py
+++ invesalius/gui/task_navigator.py
@@ -169,6 +169,7 @@ class InnerFoldPanel(wx.Panel):
         checkcamera.SetToolTip(tooltip)
         checkcamera.SetValue(const.CAM_MODE)
         checkcamera.Bind(wx.EVT_CHECKBOX, self.OnVolumeCamera)
+        checkcamera.SetValue(1)
         self.checkcamera = checkcamera
 
         # Check box for trigger monitoring to create markers from serial port
