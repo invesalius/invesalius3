927660aa381f5511ca10b3632e2cb7a8eeeccbbd
Renan <renan_hiroshi@hotmail.com>
2017-11-24 16:03:02 -0200

    Disable ref mode for obj registration

--- invesalius/gui/dialogs.py
+++ invesalius/gui/dialogs.py
@@ -3154,6 +3154,7 @@ class ObjectCalibrationDialog(wx.Dialog):
         choice_ref.SetSelection(self.obj_ref_id)
         choice_ref.SetToolTip(tooltip)
         choice_ref.Bind(wx.EVT_COMBOBOX, self.OnChoiceRefMode)
+        choice_ref.Enable(0)
 
         # Buttons to finish or cancel object registration
         tooltip = wx.ToolTip(_(u"Registration done"))
