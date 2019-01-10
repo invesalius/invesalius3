c4dc2e6b8ec54cff0223bf8c6a4514588514d006
Thiago Franco de Moraes <totonixsame@gmail.com>
2019-01-10 15:35:31 -0200

    better hierachy of tags

--- invesalius/project.py
+++ invesalius/project.py
@@ -355,11 +355,30 @@ class Project(with_metaclass(Singleton, object)):
         s = slc.Slice()
         with h5py.File(filename, 'w') as f:
             f['image'] = s.matrix
+            f['spacing'] = s.spacing
+
+            f["invesalius_version"] = const.INVESALIUS_VERSION
+            f["date"] = datetime.datetime.now().isoformat()
+            f["compress"] = self.compress
+            f["name"] = self.name # patient's name
+            f["modality"] = self.modality # CT, RMI, ...
+            f["orientation"] = self.original_orientation
+            f["window_width"] = self.window
+            f["window_level"] = self.level
+            f["scalar_range"] = self.threshold_range
+
             for index in self.mask_dict:
                 mask = self.mask_dict[index]
                 s.do_threshold_to_all_slices(mask)
-                key = 'mask_{}_{}'.format(index, mask.name)
-                f[key] = mask.matrix[1:, 1:, 1:]
+                key = 'masks/{}'.format(index)
+                f[key + '/name'] = mask.name
+                f[key + '/matrix'] = mask.matrix[1:, 1:, 1:]
+                f[key + '/colour'] = mask.colour[:3]
+                f[key + '/opacity'] = mask.opacity
+                f[key + '/threshold_range'] = mask.threshold_range
+                f[key + '/edition_threshold_range'] = mask.edition_threshold_range
+                f[key + '/visible'] = mask.is_shown
+                f[key + '/edited'] = mask.was_edited
 
 
 def Compress(folder, filename, filelist, compress=False):
