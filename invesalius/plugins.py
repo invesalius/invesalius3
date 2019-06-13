6d97175aea119e9b9fdbb4f1861e6111f28b01a2
Thiago Franco de Moraes <totonixsame@gmail.com>
2019-06-13 16:48:11 -0300

    importing plugin module

--- invesalius/plugins.py
+++ invesalius/plugins.py
@@ -1,10 +1,10 @@
-#--------------------------------------------------------------------
+# --------------------------------------------------------------------
 # Software:     InVesalius - Software de Reconstrucao 3D de Imagens Medicas
 # Copyright:    (C) 2001  Centro de Pesquisas Renato Archer
 # Homepage:     http://www.softwarepublico.gov.br
 # Contact:      invesalius@cti.gov.br
 # License:      GNU - GPL 2 (LICENSE.txt/LICENCA.txt)
-#--------------------------------------------------------------------
+# --------------------------------------------------------------------
 #    Este programa e software livre; voce pode redistribui-lo e/ou
 #    modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
 #    publicada pela Free Software Foundation; de acordo com a versao 2
@@ -15,41 +15,54 @@
 #    COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
 #    PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
 #    detalhes.
-#--------------------------------------------------------------------
+# --------------------------------------------------------------------
 
+import importlib.util
 import json
+
 from wx.lib.pubsub import pub as Publisher
 
 import invesalius.constants as consts
 from invesalius import inv_paths
 
 
+def import_source(module_name, module_file_path):
+    module_spec = importlib.util.spec_from_file_location(module_name, module_file_path)
+    module = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(module)
+    return module
+
+
 class PluginManager:
     def __init__(self):
         self.plugins = {}
+        self.__bind_pubsub_evt()
 
     def __bind_pubsub_evt(self):
-        Publisher.subscribe(self.load_plugin, 'Load plugin')
+        Publisher.subscribe(self.load_plugin, "Load plugin")
 
     def find_plugins(self):
         self.plugins = {}
         for p in sorted(inv_paths.USER_PLUGINS_DIRECTORY.glob("*")):
             if p.is_dir():
                 try:
-                    with p.joinpath('plugin.json').open() as f:
+                    with p.joinpath("plugin.json").open() as f:
                         jdict = json.load(f)
                         plugin_name = jdict["name"]
                         plugin_description = jdict["description"]
 
                         self.plugins[plugin_name] = {
                             "description": plugin_description,
-                            "folder": p
+                            "folder": p,
                         }
                 except Exception as err:
                     print("It was not possible to load plugin. Error: {}".format(err))
 
         Publisher.sendMessage("Add plugins menu items", items=self.plugins)
 
-    def load_plugin(self, name):
-        if name in self.plugins:
-            print("NAME")
+    def load_plugin(self, plugin_name):
+        if plugin_name in self.plugins:
+            plugin_module = import_source(
+                plugin_name, self.plugins[plugin_name]["folder"].joinpath("__init__.py")
+            )
+            print(dir(plugin_module))
