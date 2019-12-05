f7b5c0970365892fa582cc15ae6e6f7882ad6978
Victor Hugo Souza <victor.souza@aalto.fi>
2019-12-05 13:06:00 +0100

    ADD: Threads using Queue for navigation

--- invesalius/data/trekker_data.py
+++ invesalius/data/trekker_data.py
@@ -7,6 +7,8 @@ from wx.lib.pubsub import pub as Publisher
 import vtk
 import math
 
+import invesalius.data.bases as db
+
 
 def compute_seed(position, affine):
     pos_world_aux = np.ones([4, 1])
@@ -56,14 +58,14 @@ def compute_tubes_vtk(trk, direc):
     trkData.GetPointData().SetScalars(colors)
 
     # make it a tube
-    # trkTube = vtk.vtkTubeFilter()
-    # trkTube.SetRadius(0.5)
-    # trkTube.SetNumberOfSides(4)
-    # trkTube.SetInputData(trkData)
-    # trkTube.Update()
+    trkTube = vtk.vtkTubeFilter()
+    trkTube.SetRadius(0.5)
+    trkTube.SetNumberOfSides(4)
+    trkTube.SetInputData(trkData)
+    trkTube.Update()
 
-    # return trkTube
-    return trkData
+    return trkTube
+    # return trkData
 
 
 def tracts_actor(out_list, affine_vtk):
@@ -235,61 +237,61 @@ class ComputeTractsParallel(threading.Thread):
                 # n += 1
 
 
-class ComputeTracts(threading.Thread):
-    """
-    Thread to update the coordinates with the fiducial points
-    co-registration method while the Navigation Button is pressed.
-    Sleep function in run method is used to avoid blocking GUI and
-    for better real-time navigation
-    """
-
-    def __init__(self, tracker, position, affine, affine_vtk, n_tracts):
-        threading.Thread.__init__(self)
-        # trekker variables
-        self.tracker = tracker
-        self.position = position
-        self.affine = affine
-        self.affine_vtk = affine_vtk
-        self.n_tracts = n_tracts
-        # threading variables
-        # self.run_id = run_id
-        self._pause_ = False
-        self.start()
-
-    def stop(self):
-        self._pause_ = True
-
-    def run(self):
-
-        seed = compute_seed(self.position, self.affine)
-
-        # Compute the tracts
-        trk_list = []
-        for n in range(self.n_tracts):
-            self.tracker.set_seeds(seed)
-            if self.tracker.run()[0]:
-                trk_list.append(self.tracker.run()[0])
-
-        # Transform tracts to array
-        trk_arr = [np.asarray(trk_n).T if trk_n else None for trk_n in trk_list]
-
-        # Compute the directions
-        trk_dir = [simple_direction(trk_n) for trk_n in trk_arr]
-
-        # Compute the vtk tubes
-        out_list = [compute_tubes_vtk(trk_arr_n, trk_dir_n) for trk_arr_n, trk_dir_n in zip(trk_arr, trk_dir)]
-
-        # Compute the actor
-        actor = tracts_actor(out_list, affine_vtk=self.affine_vtk)
-
-        wx.CallAfter(Publisher.sendMessage, 'Update tracts', flag=True, actor=actor)
-
-        # time.sleep(1.)
-
-        if self._pause_:
-            return
-        else:
-            return actor
+# class ComputeTracts(threading.Thread):
+#     """
+#     Thread to update the coordinates with the fiducial points
+#     co-registration method while the Navigation Button is pressed.
+#     Sleep function in run method is used to avoid blocking GUI and
+#     for better real-time navigation
+#     """
+#
+#     def __init__(self, tracker, position, affine, affine_vtk, n_tracts):
+#         threading.Thread.__init__(self)
+#         # trekker variables
+#         self.tracker = tracker
+#         self.position = position
+#         self.affine = affine
+#         self.affine_vtk = affine_vtk
+#         self.n_tracts = n_tracts
+#         # threading variables
+#         # self.run_id = run_id
+#         self._pause_ = False
+#         self.start()
+#
+#     def stop(self):
+#         self._pause_ = True
+#
+#     def run(self):
+#
+#         seed = compute_seed(self.position, self.affine)
+#
+#         # Compute the tracts
+#         trk_list = []
+#         for n in range(self.n_tracts):
+#             self.tracker.set_seeds(seed)
+#             if self.tracker.run()[0]:
+#                 trk_list.append(self.tracker.run()[0])
+#
+#         # Transform tracts to array
+#         trk_arr = [np.asarray(trk_n).T if trk_n else None for trk_n in trk_list]
+#
+#         # Compute the directions
+#         trk_dir = [simple_direction(trk_n) for trk_n in trk_arr]
+#
+#         # Compute the vtk tubes
+#         out_list = [compute_tubes_vtk(trk_arr_n, trk_dir_n) for trk_arr_n, trk_dir_n in zip(trk_arr, trk_dir)]
+#
+#         # Compute the actor
+#         root = tracts_root(out_list, root, n_tracts)
+#
+#         wx.CallAfter(Publisher.sendMessage, 'Update tracts', flag=True, actor=actor)
+#
+#         # time.sleep(1.)
+#
+#         if self._pause_:
+#             return
+#         else:
+#             return actor
 
 
 class ComputeTractsSimple:
@@ -332,6 +334,76 @@ class ComputeTractsSimple:
         actor = tracts_actor(out_list, affine_vtk=self.affine_vtk)
 
         return actor
+
+
+def TractsThread(inp, queue_coord, queue_tract, event):
+    tracker, affine, timestamp = inp
+    p_old = np.array([[0., 0., 0.]])
+
+    # n_tracts, tracker = inp
+    print("Start compute_tracts\n")
+    """Pretend we're getting a number from the network."""
+    # While the event is not set or the queue is not empty
+    # or not queue_tract.full()
+    while not event.is_set() or not queue_coord.empty() or not queue_tract.full():
+        position, arg = queue_coord.get()
+        wx, wy, wz = db.flip_x(position[:3])
+        p2 = db.flip_x(position[:3])
+
+        if np.any(arg):
+            m_img2 = arg.copy()
+            m_img2[:3, -1] = np.asmatrix(db.flip_x_m((m_img2[0, -1], m_img2[1, -1], m_img2[2, -1]))).reshape([3, 1])
+            norm_vec = m_img2[:3, 2].reshape([1, 3]).tolist()
+            p0 = m_img2[:3, -1].reshape([1, 3]).tolist()
+            p2 = [x - timestamp * y for x, y in zip(p0[0], norm_vec[0])]
+            wx, wy, wz = p2
+            dist = abs(np.linalg.norm(p_old - np.asarray(p2)))
+            p_old = np.asarray(p2)
+
+        if dist > 3:
+            seed = compute_seed((wx, wy, wz), affine)
+            chunck_size = 6
+            tracker.set_seeds(np.repeat(seed, chunck_size, axis=0))
+
+            if tracker.run():
+                trk_list = tracker.run()
+        else:
+            trk_list = []
+        queue_tract.put(trk_list)
+
+
+def tracts_computation(trk_list, root, n_tracts):
+    # Transform tracts to array
+    trk_arr = [np.asarray(trk_n).T if trk_n else None for trk_n in trk_list]
+
+    # Compute the directions
+    trk_dir = [simple_direction(trk_n) for trk_n in trk_arr]
+
+    # Compute the vtk tubes
+    out_list = [compute_tubes_vtk(trk_arr_n, trk_dir_n) for trk_arr_n, trk_dir_n in zip(trk_arr, trk_dir)]
+
+    root = tracts_root(out_list, root, n_tracts)
+
+    return root
+
+
+def ComputeTractsChunck(affine_vtk, queue, event):
+
+    n_tracts = 0
+    root = vtk.vtkMultiBlockDataSet()
+
+    while not event.is_set() or not queue.empty():
+    # while not event.is_set():
+        # time.sleep(0.5)
+        print("Enter split_simple\n")
+        # trk_list = queue.get_nowait()
+        trk_list = queue.get()
+        root = tracts_computation(trk_list, root, n_tracts)
+        n_tracts += len(trk_list)
+
+        wx.CallAfter(Publisher.sendMessage, 'Update tracts', flag=True, root=root, affine_vtk=affine_vtk)
+
+        # return actor
 
 
 class ComputeTractsDev:
