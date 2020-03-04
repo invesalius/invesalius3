FIX: Navigation with tractography working - Improved offset computation for seed - Improved flip_x. No need to use a function, only multiply Y coordinate by -1

invesalius/data/trekker_data.py
@@ -120,6 +120,7 @@ class ComputeVisualizeParallel(threading.Thread):
         trekker, affine, offset, n_tracts_total, seed_radius, n_threads = self.inp
         p_old = np.array([[0., 0., 0.]])
         n_tracts = 0
+        count = 0
         # ncores = psutil.cpu_count()
         # chunck_size = 2*ncores
         trk_list = []
@@ -133,7 +134,15 @@ class ComputeVisualizeParallel(threading.Thread):
                 if np.any(m_img):
                     # #TODO: Refactor and create a function for the offset computation
                     # # m_img[:3, -1] = np.asmatrix(db.flip_x_m((m_img[0, -1], m_img[1, -1], m_img[2, -1]))).reshape([3, 1])
-                    m_img[:, -1] = db.flip_x_m(m_img[:3, -1])[:, 0]
+
+                    # in this new refactored version the m_img comes different than the position
+                    # the new version m_img is already flixped in y, which means that Y is negative
+                    # if only the Y is negative maybe no need for the flip_x funtcion at all in the navigation
+                    # but check all pipeline before why now the m_img comes different than position
+                    m_img_flip = m_img.copy()
+                    # m_img_flip[:, -1] = db.flip_x_m(m_img_flip[:3, -1])[:, 0]
+                    m_img_flip[1, -1] = -m_img_flip[1, -1]
+
                     # # norm_vec = m_img[:3, 2].reshape([1, 3]).tolist()
                     # # p0 = m_img[:3, -1].reshape([1, 3]).tolist()
                     # # p_new = [x - offset * y for x, y in zip(p0[0], norm_vec[0])]
@@ -151,43 +160,51 @@ class ComputeVisualizeParallel(threading.Thread):
                     # tracker.seed_coordinates(np.tile(seed, (n_threads, 1)))
                     #
                     # print("Trekker run: {}".format(tracker.run()))
-
+                    p_new = m_img_flip[:3, -1] - offset * m_img_flip[:3, 2]
                     # m_img[:3, -1] = np.asmatrix(db.flip_x_m((m_img[0, -1], m_img[1, -1], m_img[2, -1]))).reshape([3, 1])
-                    norm_vec = m_img[:3, 2].reshape([1, 3]).tolist()
-                    p0 = m_img[:3, -1].reshape([1, 3]).tolist()
-                    p_new = [x - offset * y for x, y in zip(p0[0], norm_vec[0])]
+                    # norm_vec = m_img_flip[:3, 2].reshape([1, 3]).tolist()
+                    # p0 = m_img_flip[:3, -1].reshape([1, 3]).tolist()
+                    # p_new = [x - offset * y for x, y in zip(p0[0], norm_vec[0])]
                     # p_new = np.array([[27.53, -77.37, 46.42]])
                     dist = abs(np.linalg.norm(p_old - np.asarray(p_new)))
-                    p_old = np.asarray(p_new)
+                    p_old = p_new.copy()
 
-                    seed = compute_seed_old(p_new, affine)
+                    # seed = compute_seed_old(p_new, affine)
+                    seed = compute_seed(p_new, affine)
                     # Juuso's
                     # seed = np.array([[-8.49, -8.39, 2.5]])
                     # Baran M1
                     # seed = np.array([[27.53, -77.37, 46.42]])
+                    # print("Seed: {}".format(seed))
                     trekker.seed_coordinates(np.repeat(seed, n_threads, axis=0))
 
+                    # wx.CallAfter(Publisher.sendMessage, 'Update cross position', arg=m_img, position=position)
+                    # wx.CallAfter(Publisher.sendMessage, 'Update object matrix', m_img=m_img, coord=position)
+
                     if trekker.run():
+                        # print("dist: {}".format(dist))
                         if dist >= seed_radius:
                             n_tracts = 0
                             trk_list = trekker.run()
-                            print("trk list len: ", len(trk_list))
+                            # print("New tracts: ", len(trk_list))
                             root = tracts_computation(trk_list, root, n_tracts)
                             n_tracts = len(trk_list)
                             # print("Total tracts: ", n_tracts)
-                            # wx.CallAfter(Publisher.sendMessage, 'Remove tracts')
+                            count += 1
+                            wx.CallAfter(Publisher.sendMessage, 'Remove tracts', count=count)
                             wx.CallAfter(Publisher.sendMessage, 'Update tracts', flag=True, root=root,
-                                         affine_vtk=self.affine_vtk)
+                                         affine_vtk=self.affine_vtk, count=count)
                         elif dist < seed_radius and n_tracts < n_tracts_total:
                             # Compute the tracts
                             trk_list.extend(trekker.run())
                             # print("Menor que seed_radius com n tracts and dist: ", n_tracts, dist)
                             root = tracts_computation(trk_list, root, n_tracts)
                             n_tracts = len(trk_list)
-                            print("Total new tracts: ", n_tracts)
-                            # wx.CallAfter(Publisher.sendMessage, 'Remove tracts')
+                            # print("Adding tracts: ", n_tracts)
+                            count += 1
+                            wx.CallAfter(Publisher.sendMessage, 'Remove tracts', count=count)
                             wx.CallAfter(Publisher.sendMessage, 'Update tracts', flag=True, root=root,
-                                         affine_vtk=self.affine_vtk)
+                                         affine_vtk=self.affine_vtk, count=count)
 
                         # this logic is a bit stupid because it has to compute the actors every loop, better would be
                         # to check the distance and update actors in viewer volume, but that would require that each

invesalius/data/viewer_volume.py
@@ -1353,10 +1353,12 @@ class Viewer(wx.Panel):
         # m_tract = m_img_copy.copy()
         # m_tract[:3, -1] = np.reshape(np.asarray(p2)[np.newaxis, :], [3, 1])
 
-        m_img[:, -1] = bases.flip_x_m(m_img[:3, -1])[:, 0]
+        m_img_flip = m_img.copy()
+        # m_img_flip[:, -1] = bases.flip_x_m(m_img_flip[:3, -1])[:, 0]
+        m_img_flip[1, -1] = -m_img_flip[1, -1]
 
         # translate coregistered coordinate to display a marker where Trekker seed is computed
-        m_tract = m_img.copy()
+        m_tract = m_img_flip.copy()
         # m_tract[:, -1] = bases.flip_x_m(m_img[:3, -1])[:, 0]
         m_tract[:3, -1] = m_tract[:3, -1] - self.seed_offset * m_tract[:3, 2]
 
@@ -1371,7 +1373,7 @@ class Viewer(wx.Panel):
 
         for row in range(0, 4):
             for col in range(0, 4):
-                m_img_vtk.SetElement(row, col, m_img[row, col])
+                m_img_vtk.SetElement(row, col, m_img_flip[row, col])
                 m_tract_vtk.SetElement(row, col, m_tract[row, col])
 
         self.obj_actor.SetUserMatrix(m_img_vtk)
@@ -1416,7 +1418,7 @@ class Viewer(wx.Panel):
     # def UpdateAffineWorld2Inv(self, affine, status):
     #     self.affine = affine
 
-    def OnUpdateTracts(self, evt=None, flag=None, actor=None, root=None, affine_vtk=None):
+    def OnUpdateTracts(self, evt=None, flag=None, actor=None, root=None, affine_vtk=None, count=0):
         # TODO: Remove tracts also when cross button is untoogled
 
         # new method to update real time the actor
@@ -1426,7 +1428,7 @@ class Viewer(wx.Panel):
             #     self.Refresh()
             # if root:
         # https://lorensen.github.io/VTKExamples/site/Python/CompositeData/CompositePolyDataMapper/
-        self.ren.RemoveActor(self.actor_tracts)
+        # self.ren.RemoveActor(self.actor_tracts)
 
         mapper = vtk.vtkCompositePolyDataMapper2()
         mapper.SetInputDataObject(root)
@@ -1435,10 +1437,12 @@ class Viewer(wx.Panel):
         self.actor_tracts.SetMapper(mapper)
         self.actor_tracts.SetUserMatrix(affine_vtk)
 
+        # print("Add count: ", count)
         self.ren.AddActor(self.actor_tracts)
         self.Refresh()
 
-    def OnRemoveTracts(self, evt=None):
+    def OnRemoveTracts(self, evt=None, count=0):
+        # print("Remove count: ", count)
         self.ren.RemoveActor(self.actor_tracts)
         self.Refresh()
 