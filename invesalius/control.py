0ea8820aee0a177ee2bf667fa15388f4ab64a25c
Thiago Franco de Moraes <totonixsame@gmail.com>
2020-04-09 16:41:40 -0300

    When importing non DICOM files the filepath was being used as project name

--- invesalius/control.py
+++ invesalius/control.py
@@ -872,13 +872,11 @@ class Controller():
     def OnOpenOtherFiles(self, filepath):
         filepath = utils.decode(filepath, const.FS_ENCODE)
         if not(filepath) == None:
-            name = filepath.rpartition('\\')[-1].split('.')
-
+            name = os.path.basename(filepath).split(".")[0]
             group = oth.ReadOthers(filepath)
-            
             if group:
                 matrix, matrix_filename = self.OpenOtherFiles(group)
-                self.CreateOtherProject(str(name[0]), matrix, matrix_filename)
+                self.CreateOtherProject(name, matrix, matrix_filename)
                 self.LoadProject()
                 Publisher.sendMessage("Enable state project", state=True)
             else:
