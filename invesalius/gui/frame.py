7fae73871e9749ab0ca263bd50498fa49f9bdc9a
Thiago Franco de Moraes <totonixsame@gmail.com>
2021-07-26 13:54:17 -0300

    Ctrl+Shift+M -> Ctrl+Shift+P to preview mask in 3D

--- invesalius/gui/frame.py
+++ invesalius/gui/frame.py
@@ -1008,7 +1008,7 @@ class MenuBar(wx.MenuBar):
 
         mask_preview_menu = wx.Menu()
 
-        self.mask_preview = mask_preview_menu.Append(const.ID_MASK_3D_PREVIEW, _("Enable") + "\tCtrl+Shift+M", "", wx.ITEM_CHECK)
+        self.mask_preview = mask_preview_menu.Append(const.ID_MASK_3D_PREVIEW, _("Enable") + "\tCtrl+Shift+P", "", wx.ITEM_CHECK)
         self.mask_preview.Enable(False)
 
         self.mask_auto_reload = mask_preview_menu.Append(const.ID_MASK_3D_AUTO_RELOAD, _("Auto reload") + "\tCtrl+Shift+D", "", wx.ITEM_CHECK)
