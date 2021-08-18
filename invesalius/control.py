214fc22820c7692f1408d2ce1d44c32e9699f7fd
Thiago Franco de Moraes <totonixsame@gmail.com>
2021-08-18 15:07:22 -0300

    Checking if any spacing value is zero and showing a dialog to user set a valid spacing

--- invesalius/control.py
+++ invesalius/control.py
@@ -489,6 +489,8 @@ class Controller():
             # OPTION 1: DICOM
             group = dcm.SelectLargerDicomGroup(patients_groups)
             matrix, matrix_filename, dicom = self.OpenDicomGroup(group, 0, [0, 0], gui=gui)
+            if matrix is None:
+                return
             self.CreateDicomProject(dicom, matrix, matrix_filename)
         else:
             # OPTION 2: NIfTI, Analyze or PAR/REC
@@ -517,6 +519,8 @@ class Controller():
     def ImportGroup(self, group, gui=True):
 
         matrix, matrix_filename, dicom = self.OpenDicomGroup(group, 0, [0, 0], gui=gui)
+        if matrix is None:
+            return
         self.CreateDicomProject(dicom, matrix, matrix_filename)
 
         self.LoadProject()
@@ -896,6 +900,8 @@ class Controller():
             if dlg.ShowModal() != wx.ID_YES:
                 return
         matrix, matrix_filename, dicom = self.OpenDicomGroup(group, interval, file_range, gui=True)
+        if matrix is None:
+            return
         self.CreateDicomProject(dicom, matrix, matrix_filename)
         self.LoadProject()
         Publisher.sendMessage("Enable state project", state=True)
@@ -980,6 +986,14 @@ class Controller():
         self.Slice = sl.Slice()
         self.Slice.matrix = self.matrix
         self.Slice.matrix_filename = self.filename
+
+        if gui and (spacing[0] == 0.0 or spacing[1] == 0.0 or spacing[2] == 0.0):
+            sx, sy, sz = spacing
+            dlg = dialogs.SetSpacingDialog(wx.GetApp().GetTopWindow(), sx, sy, sz)
+            if dlg.ShowModal() == wx.ID_OK:
+                spacing = dlg.spacing_new_x, dlg.spacing_new_y, dlg.spacing_new_z
+            else:
+                return None, None, None
 
         self.Slice.spacing = spacing
 
--- invesalius/gui/dialogs.py
+++ invesalius/gui/dialogs.py
@@ -4369,3 +4369,98 @@ class ManualWWWLDialog(wx.Dialog):
 
     def OnClose(self, evt):
         self.Destroy()
+
+
+
+class SetSpacingDialog(wx.Dialog):
+    def __init__(
+        self,
+        parent,
+        sx,
+        sy,
+        sz,
+        title=_("Set spacing"),
+        style=wx.DEFAULT_DIALOG_STYLE | wx.FRAME_FLOAT_ON_PARENT | wx.STAY_ON_TOP,
+    ):
+        wx.Dialog.__init__(self, parent, -1, title=title, style=style)
+        self.spacing_original_x = sx
+        self.spacing_original_y = sy
+        self.spacing_original_z = sz
+
+        self._init_gui()
+        self._bind_events()
+
+    def _init_gui(self):
+        self.txt_spacing_new_x = wx.TextCtrl(self, -1, value=str(self.spacing_original_x))
+        self.txt_spacing_new_y = wx.TextCtrl(self, -1, value=str(self.spacing_original_y))
+        self.txt_spacing_new_z = wx.TextCtrl(self, -1, value=str(self.spacing_original_z))
+
+        sizer_new = wx.FlexGridSizer(3, 2, 5, 5)
+        sizer_new.AddMany(
+            (
+                (wx.StaticText(self, -1, "Spacing X"), 0, wx.ALIGN_CENTER_VERTICAL),
+                (self.txt_spacing_new_x, 1, wx.EXPAND),
+                (wx.StaticText(self, -1, "Spacing Y"), 0, wx.ALIGN_CENTER_VERTICAL),
+                (self.txt_spacing_new_y, 1, wx.EXPAND),
+                (wx.StaticText(self, -1, "Spacing Z"), 0, wx.ALIGN_CENTER_VERTICAL),
+                (self.txt_spacing_new_z, 1, wx.EXPAND),
+            )
+        )
+
+        self.button_ok = wx.Button(self, wx.ID_OK)
+        self.button_cancel = wx.Button(self, wx.ID_CANCEL)
+
+        button_sizer = wx.StdDialogButtonSizer()
+        button_sizer.AddButton(self.button_ok)
+        button_sizer.AddButton(self.button_cancel)
+        button_sizer.Realize()
+
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(wx.StaticText(self, -1, _("It was not possible to obtain the image spacings.\nPlease set it correctly:")), 0, wx.EXPAND)
+        main_sizer.Add(sizer_new, 1, wx.EXPAND | wx.ALL, 5)
+        main_sizer.Add(button_sizer, 0, wx.EXPAND | wx.TOP | wx.BOTTOM, 5)
+
+        self.SetSizer(main_sizer)
+        main_sizer.Fit(self)
+        self.Layout()
+
+    def _bind_events(self):
+        self.txt_spacing_new_x.Bind(wx.EVT_KILL_FOCUS, self.OnSetNewSpacing)
+        self.txt_spacing_new_y.Bind(wx.EVT_KILL_FOCUS, self.OnSetNewSpacing)
+        self.txt_spacing_new_z.Bind(wx.EVT_KILL_FOCUS, self.OnSetNewSpacing)
+
+        self.button_ok.Bind(wx.EVT_BUTTON, self.OnOk)
+        self.button_cancel.Bind(wx.EVT_BUTTON, self.OnCancel)
+
+    def OnSetNewSpacing(self, evt):
+        try:
+            new_spacing_x = float(self.txt_spacing_new_x.GetValue())
+        except ValueError:
+            new_spacing_x = self.spacing_new_x
+
+        try:
+            new_spacing_y = float(self.txt_spacing_new_y.GetValue())
+        except ValueError:
+            new_spacing_y = self.spacing_new_y
+
+        try:
+            new_spacing_z = float(self.txt_spacing_new_z.GetValue())
+        except ValueError:
+            new_spacing_z = self.spacing_new_z
+
+        self.set_new_spacing(new_spacing_x, new_spacing_y, new_spacing_z)
+
+    def set_new_spacing(self, sx, sy, sz):
+        self.spacing_new_x = sx
+        self.spacing_new_y = sy
+        self.spacing_new_z = sz
+
+        self.txt_spacing_new_x.ChangeValue(str(sx))
+        self.txt_spacing_new_y.ChangeValue(str(sy))
+        self.txt_spacing_new_z.ChangeValue(str(sz))
+
+    def OnOk(self, evt):
+        self.EndModal(wx.ID_OK)
+
+    def OnCancel(self, evt):
+        self.EndModal(wx.ID_CANCEL)
