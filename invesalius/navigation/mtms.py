4001d1d9ce2b57ed666479c8ad0eca06d525ef24
Renan <renan_hiroshi@hotmail.com>
2022-08-08 10:25:12 +0300

    FIX: mtms api

--- invesalius/navigation/mtms.py
+++ invesalius/navigation/mtms.py
@@ -1,13 +1,14 @@
 import win32com.client
 
 import numpy as np
+import time
 
 import invesalius.data.coordinates as dco
 import invesalius.data.coregistration as dcr
 
 class mTMS():
     def __init__(self):
-        mtms_path = 'D:\\mTMS 3.1'
+        mtms_path = 'C:\\mTMS\\Labview\\Builds\\mTMS 3.1'
         vipath = mtms_path + '\\mTMS ActiveX Server\\mTMS ActiveX Server.exe\\mTMS ActiveX Server.vi'
         # Connect to the ActiveX server
         mtms_app = win32com.client.Dispatch('MTMSActiveXServer.Application')
@@ -32,8 +33,10 @@ class mTMS():
         print(offset)
         mTMS_index_target = self.FindmTMSParameters([int(x) for x in offset])
         print(mTMS_index_target)
-        if mTMS_index_target is not None:
-            self.SendToMTMS(mTMS_index_target)
+        if len(mTMS_index_target[0]):
+            self.SendToMTMS(mTMS_index_target[0])
+        else:
+            print("Target is not valid")
 
     def GetOffset(self, distance):
         print(distance)
@@ -43,7 +46,7 @@ class mTMS():
         return [offset_xy[0], offset_xy[1], offset_rz]
 
     def FindmTMSParameters(self, offset):
-        fname = 'C:\\repository\\efield_analysis\\PP31 5-coil grid.txt'
+        fname = 'C:\\mTMS\\mTMS parameters\\PP\\PP31 5-coil grid.txt'
 
         with open(fname, 'r') as the_file:
             all_data = [line.strip() for line in the_file.readlines()]
@@ -58,15 +61,20 @@ class mTMS():
     def SendToMTMS(self, target):
         # Manipulate intensity
         intensity = self.vi.GetControlValue('Get Intensity')
-        print("Initial intensity: ", str(intensity))
+        print("Intensity: ", str(intensity))
         #self.vi.SetControlValue('New Intensity', 40)
         #self.vi.SetControlValue('Set Intensity', True)
 
         # Update the Pulse - parameters row and wait until the change has been processed
-        self.vi.SetControlValue('New Pulse-parameters row', target);
+        self.vi.SetControlValue('New Pulse-parameters row', int(target));
         self.vi.SetControlValue('Set Pulse-parameters row', True);
+        print("Updating brain target...")
         while self.vi.GetControlValue('Set Pulse-parameters row'):
-            print("Updating brain target...")
-
+            pass
+        time.sleep(0.1)
+        print("Charging capacitors...")
+        while not self.vi.GetControlValue('Get Ready to stimulate'):
+            pass
         # Stimulate
+        print("Stimulating")
         self.vi.SetControlValue('Stimulate', True)
