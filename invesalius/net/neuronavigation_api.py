03a32725316371bb4a9999a5d5b6e3234ad42bca
sotodela <ana.sotodelacruz@gmail.com>
2023-04-27 14:12:17 +0300

    ADD: New ROS services interface

--- invesalius/net/neuronavigation_api.py
+++ invesalius/net/neuronavigation_api.py
@@ -131,13 +131,27 @@ class NeuronavigationApi(metaclass=Singleton):
                 state=state
             )
 
-    def init_efield(self, cortexfile, meshfile):
+    def init_efield(self, cortexfile, meshfile,coilfile):
         if self.connection is not None:
             return self.connection.init_efield(
                 cortexfile=cortexfile,
-                meshfile= meshfile
+                meshfile= meshfile,
+                coilfile =coilfile
             )
         return None
+
+    def init_efield_config_file(self, config_file):
+        if self.connection is not None:
+            return self.connection.init_efield_json(
+                config_file=config_file
+            )
+        return None
+
+    def efield_coil(self, coilfile):
+        if self.connection is not None:
+            return self.connection.efield_coil(
+                coilfile= coilfile
+            )
 
     def update_efield(self, position, orientation, T_rot):
         if self.connection is not None:
