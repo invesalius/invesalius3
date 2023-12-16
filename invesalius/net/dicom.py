e5516909638e3b85179045c825afd149b018f74a
vinicius <vhpcavalcanti@outlook.com>
2023-12-16 10:26:17 -0300

    fix(PACS): create multi datasets on query to avoid conflict

--- invesalius/net/dicom.py
+++ invesalius/net/dicom.py
@@ -87,47 +87,51 @@ class DicomNet:
 
         patients = {}
 
-        ds = Dataset()
-        ds.QueryRetrieveLevel = 'PATIENT'
-        ds.PatientName = f'*{self.search_word}*'
-        ds.PatientID = ''
+        patient_ds = Dataset()
+        patient_ds.QueryRetrieveLevel = 'PATIENT'
+        patient_ds.PatientName = f'*{self.search_word}*'
+        patient_ds.PatientID = ''
 
         patientsId = []
         response = assoc.send_c_find(
-            ds, PatientRootQueryRetrieveInformationModelFind)
+            patient_ds, PatientRootQueryRetrieveInformationModelFind)
         for (patient_status, patient_identifier) in response:
 
             if patient_status and patient_status.Status in (0xFF00, 0xFF01):
 
                 patientsId.append(patient_identifier.get('PatientID', None))
 
-        patientStudies = {}
-        for id in patientsId:
+        for patientId in patientsId:
 
-            ds.QueryRetrieveLevel = 'STUDY'
-            ds.PatientID = id
-            ds.StudyInstanceUID = ''
+            patientStudies = {}
+
+            study_ds = Dataset()
+            study_ds.QueryRetrieveLevel = 'STUDY'
+            study_ds.PatientID = patientId
+            study_ds.StudyInstanceUID = ''
             response = assoc.send_c_find(
-                ds, PatientRootQueryRetrieveInformationModelFind)
+                study_ds, PatientRootQueryRetrieveInformationModelFind)
             for (status, identifier) in response:
 
                 if status and status.Status in (0xFF00, 0xFF01):
 
-                    if not id in patientStudies.keys():
-                        patientStudies[id] = []
+                    if not patientId in patientStudies.keys():
+                        patientStudies[patientId] = []
 
-                    patientStudies[id].append(
+                    patientStudies[patientId].append(
                         identifier.get('StudyInstanceUID', None))
 
-            studySeries = {}
-            for study_id in patientStudies[id]:
+            for study_id in patientStudies[patientId]:
 
-                ds.QueryRetrieveLevel = 'SERIES'
-                ds.PatientID = id
-                ds.StudyInstanceUID = study_id
-                ds.SeriesInstanceUID = ''
+                studySeries = {}
+
+                series_ds = Dataset()
+                series_ds.QueryRetrieveLevel = 'SERIES'
+                series_ds.PatientID = patientId
+                series_ds.StudyInstanceUID = study_id
+                series_ds.SeriesInstanceUID = ''
                 response = assoc.send_c_find(
-                    ds, PatientRootQueryRetrieveInformationModelFind)
+                    series_ds, PatientRootQueryRetrieveInformationModelFind)
                 for (status, identifier) in response:
 
                     if status and status.Status in (0xFF00, 0xFF01):
@@ -140,33 +144,34 @@ class DicomNet:
 
                 for serie_id in studySeries[study_id]:
 
-                    ds.QueryRetrieveLevel = 'IMAGE'
-                    ds.PatientID = id
-                    ds.StudyInstanceUID = study_id
-                    ds.SeriesInstanceUID = serie_id
-                    ds.SOPInstanceUID = ''
-                    ds.PatientName = ''
-                    ds.PatientBirthDate = ''
-                    ds.PatientAge = ''
-                    ds.PatientSex = ''
-                    ds.StudyDescription = ''
-                    ds.InstitutionName = ''
-                    ds.Modality = ''
-                    ds.AccessionNumber = ''
-                    ds.ReferringPhysicianName = ''
-                    ds.SeriesDescription = ''
-                    ds.AcquisitionTime = ''
-                    ds.AcquisitionDate = ''
+                    image_ds = Dataset()
+                    image_ds.QueryRetrieveLevel = 'IMAGE'
+                    image_ds.PatientID = patientId
+                    image_ds.StudyInstanceUID = study_id
+                    image_ds.SeriesInstanceUID = serie_id
+                    image_ds.SOPInstanceUID = ''
+                    image_ds.PatientName = ''
+                    image_ds.PatientBirthDate = ''
+                    image_ds.PatientAge = ''
+                    image_ds.PatientSex = ''
+                    image_ds.StudyDescription = ''
+                    image_ds.InstitutionName = ''
+                    image_ds.Modality = ''
+                    image_ds.AccessionNumber = ''
+                    image_ds.ReferringPhysicianName = ''
+                    image_ds.SeriesDescription = ''
+                    image_ds.AcquisitionTime = ''
+                    image_ds.AcquisitionDate = ''
                     response = assoc.send_c_find(
-                        ds, PatientRootQueryRetrieveInformationModelFind)
+                        image_ds, PatientRootQueryRetrieveInformationModelFind)
                     for (status, identifier) in response:
 
                         if status and status.Status in (0xFF00, 0xFF01):
 
-                            if not (id in patients.keys()):
-                                patients[id] = {}
+                            if not (patientId in patients.keys()):
+                                patients[patientId] = {}
 
-                            if not (serie_id in patients[id]):
+                            if not (serie_id in patients[patientId]):
 
                                 name = identifier.get('PatientName', None)
                                 age = identifier.get(
@@ -183,8 +188,8 @@ class DicomNet:
                                     'InstitutionName', None)
                                 date_of_birth = identifier.get(
                                     'PatientBirthDate', None)
-                                date_of_birth = datetime.strptime(
-                                    date_of_birth, '%Y%m%d').strftime('%d/%m/%Y') if date_of_birth else ''
+                                date_of_birth = self._date_format(
+                                    date_of_birth) if date_of_birth else ''
                                 acession_number = identifier.get(
                                     'AccessionNumber', None)
                                 ref_physician = identifier.get(
@@ -193,28 +198,28 @@ class DicomNet:
                                     'SeriesDescription', None)
                                 acquisition_time = identifier.get(
                                     'AcquisitionTime', None)
-                                acquisition_time = datetime.strptime(
-                                    acquisition_time, '%H%M%S').strftime('%H:%M:%S') if acquisition_time else ''
+                                acquisition_time = self._time_format(
+                                    acquisition_time) if acquisition_time else ''
                                 acquisition_date = identifier.get(
                                     'AcquisitionDate', None)
-                                acquisition_date = datetime.strptime(
-                                    acquisition_date, '%Y%m%d').strftime('%d/%m/%Y') if acquisition_date else ''
+                                acquisition_date = self._date_format(
+                                    acquisition_date) if acquisition_date else ''
 
-                                patients[id][serie_id] = {'name': name, 'age': age, 'gender': gender,
-                                                          'study_id': study_instance_uid,
-                                                          'study_description': study_description,
-                                                          'modality': modality,
-                                                          'acquisition_time': acquisition_time,
-                                                          'acquisition_date': acquisition_date,
-                                                          'institution': institution,
-                                                          'date_of_birth': date_of_birth,
-                                                          'acession_number': acession_number,
-                                                          'ref_physician': ref_physician,
-                                                          'serie_description': serie_description, 'n_images': 1}
+                                patients[patientId][serie_id] = {'name': name, 'age': age, 'gender': gender,
+                                                                 'study_id': study_instance_uid,
+                                                                 'study_description': study_description,
+                                                                 'modality': modality,
+                                                                 'acquisition_time': acquisition_time,
+                                                                 'acquisition_date': acquisition_date,
+                                                                 'institution': institution,
+                                                                 'date_of_birth': date_of_birth,
+                                                                 'acession_number': acession_number,
+                                                                 'ref_physician': ref_physician,
+                                                                 'serie_description': serie_description, 'n_images': 1}
 
                             else:
 
-                                patients[id][serie_id]['n_images'] += 1
+                                patients[patientId][serie_id]['n_images'] += 1
 
                     break
 
@@ -307,3 +312,15 @@ class DicomNet:
         return "Address: %s\nPort: %s\nAETitle: %s\nAETitleCall: %s\nSearchWord: %s\nSearchType: %s\n" %\
                (self.address, self.port, self.aetitle,
                 self.aetitle_call, self.search_word, self.search_type)
+
+    def _date_format(self, date):
+
+        date = date.split('.')[0] if '.' in date else date
+        date = datetime.strptime(date, '%Y%m%d').strftime('%d/%m/%Y')
+        return date
+
+    def _time_format(self, time):
+
+        time = time.split('.')[0] if '.' in time else time
+        time = datetime.strptime(time, '%H%M%S').strftime('%H:%M:%S')
+        return time
