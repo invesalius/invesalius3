135bcfc78415e446bca50c92dba255fe4b764a26
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-02-10 11:38:03 +0200

    CLP: Remove some deprecated and commented out code

--- invesalius/navigation/robot.py
+++ invesalius/navigation/robot.py
@@ -114,40 +114,12 @@ class Robot(metaclass=Singleton):
 
     def ConnectToRobot(self):
         Publisher.sendMessage('Connect to robot', robot_IP=self.robot_ip)
-        print("Connected to Robot!")
+        print("Connected to robot")
 
     def InitializeRobot(self):
         Publisher.sendMessage('Robot navigation mode', robot_mode=True)
         Publisher.sendMessage('Load robot transformation matrix', data=self.matrix_tracker_to_robot.tolist())
-        print("Robot Initialized!")
+        print("Robot initialized")
 
     def DisconnectRobot(self):
         Publisher.sendMessage('Robot navigation mode', robot_mode=False)
-
-'''
-Deprecated Code
-
-    def ConfigureRobot(self):
-        if self.tracker.tracker_connection and self.tracker.tracker_connection.IsConnected():
-            select_ip_dialog = dlg.SetRobotIP()
-            status = select_ip_dialog.ShowModal()
-
-            if status == ID_OK:
-                robot_ip = select_ip_dialog.GetValue()
-                self.robot_ip = robot_ip
-                self.configuration = {
-                    'tracker_id': self.tracker.GetTrackerId(),
-                    'robot_ip': robot_ip,
-                    'tracker_configuration': self.tracker.tracker_connection.GetConfiguration(),
-                }
-                self.connection = self.tracker.tracker_connection
-                Publisher.sendMessage('Connect to robot', robot_IP=self.robot_ip)
-                select_ip_dialog.Destroy()
-                return True
-            else:
-                select_ip_dialog.Destroy()
-                return False
-        else:
-            wx.MessageBox(_("Select Tracker first"), _("InVesalius 3"))
-            return False
-'''
