d8b38283d3876d0ecbe59d5160bc8623aab8a54a
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-02-17 22:35:07 +0200

    MOD: Align coil's front-back with anterior-posterior in 'project to scalp'

--- invesalius/data/markers/marker_transformator.py
+++ invesalius/data/markers/marker_transformator.py
@@ -1,3 +1,5 @@
+import math
+
 import vtk
 import numpy as np
 
@@ -52,7 +54,7 @@ class MarkerTransformator:
         point_locator.BuildLocator()
         closest_point_id = point_locator.FindClosestPoint(marker_position)
 
-        # Retrieve the coordinates of the closest point using the point ID
+        # Retrieve the coordinates of the closest point using the point ID.
         closest_point = polydata.GetPoint(closest_point_id)
 
         # Extract the normal at the closest point
@@ -61,39 +63,37 @@ class MarkerTransformator:
 
         # The reference direction vector that we want to align the normal to.
         #
+        # This was figured out by testing; from the vectors (1, 0, 0), (0, 1, 0), and (0, 0, 1), the one was selected that
+        # made the coil point towards the brain.
         ref_vector = np.array([0, 0, 1])
 
-        # Normal at the closest point
+        # Normal at the closest point.
         normal_vector = np.array(closest_normal)
 
-        # Calculate the rotation axis (cross product) and angle (dot product)
+        # Calculate the rotation axis (cross product) and angle (dot product).
         rotation_axis = np.cross(ref_vector, normal_vector)
         rotation_angle = np.arccos(np.dot(ref_vector, normal_vector) / (np.linalg.norm(ref_vector) * np.linalg.norm(normal_vector)))
 
-        # Normalize the rotation axis
+        # Normalize the rotation axis.
         rotation_axis_normalized = rotation_axis / np.linalg.norm(rotation_axis)
 
-        # Create a rotation matrix from the axis and angle
+        # Create a rotation matrix from the axis and angle.
         rotation_matrix = tr.rotation_matrix(rotation_angle, rotation_axis_normalized)
 
-        # Convert the rotation matrix to Euler angles
-        euler_angles = tr.euler_from_matrix(rotation_matrix, 'sxyz')
-        
-        euler_angles = np.degrees(euler_angles)
+        # Rotate around coil's z-axis an additional 90 degrees to make coil's front-back axis align with anterior-posterior axis of the brain.
+        #
+        # This was figured out by looking at the discrepancy between the coil and the brain in the 3D view.
+        rotation_matrix_align_coil = tr.rotation_matrix(math.pi / 2, [0, 0, 1])
 
-        A = dco.coordinates_to_transformation_matrix([0,0,0], euler_angles, axes='sxyz')
-        print("simot:")
-        print(A)
-        print(A @ [0,0,1,1])
-        print(normal_vector)
+        # Convert the rotation matrix to Euler angles.
+        euler_angles = tr.euler_from_matrix(rotation_matrix @ rotation_matrix_align_coil, 'sxyz')
+
+        # Convert the Euler angles to degrees.
+        euler_angles_deg = np.degrees(euler_angles)
 
         # XXX: Invert back to the get to 'marker space'.
         closest_point = list(closest_point)
         closest_point[1] = -closest_point[1]
 
         marker.position = closest_point
-        marker.orientation = euler_angles
-
-        print(marker.orientation)
-        # This has been figured out by testing; from the vectors (1, 0, 0), (0, 1, 0), and (0, 0, 1), the one was selected that
-        # made the coil point towards the brain.
+        marker.orientation = euler_angles_deg
