ec81f81c6f1edb94edddc413341c9f2d035c3cb8
Rishabh Ranjan <rishabh.ranjan@gmail.com>
2024-02-21 19:14:16 +0530

    ...

--- invesalius/data/log.py
+++ invesalius/data/log.py
@@ -1,77 +1,35 @@
 import logging 
 import logging.config 
+from typing import Callable
+import invesalius.session as ses
 
-logging.config.dictConfig(
-    { 'version': 1,
-      'disable_existing_loggers': False, # this fixes the problem 
-      'formatters': { 
-            'standard': { 
-                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-            }, 
-            'simple': {
-                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                'datefmt': '%Y-%m-%d %H:%M:%S'
-            }
-        },
-        'handlers': { 
-            'default': { 
-                'level':'INFO', 
-                'class':'logging.StreamHandler',
-            }, 
-            'console': {
-                'class': 'logging.StreamHandler',
-                'level': 'DEBUG',
-                'formatter': 'simple',
-                'stream': 'ext://sys.stdout',
-            },
-            'file': {
-                'class': 'logging.FileHandler',
-                'level': 'INFO',
-                'formatter': 'simple',
-                'filename': 'myapp.log',
-                'mode': 'a'
-            }
-        },
-        'loggers': { 
-            '': {
-                'handlers': ['default'], 
-                'level': 'INFO', 
-                'propagate': True
-            },
-            'development': {
-                'level': 'DEBUG',
-                'handlers': ['console'],
-                'propagate': False
-            },
-            'staging': {
-                'level': 'INFO',
-                'handlers': ['console', 'file'],
-                'propagate': False
-            },
-            'production': {
-                'level': 'WARNING',
-                'handlers': ['file'],
-                'propagate': False
-            }
-        },
-        'root': {
-            'level': 'DEBUG',
-            'handlers': ['console']
-        }
-    }
-)
+def configureLogging():
+    # create logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # create console handler 
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    # create file handler 
+    logging.config.fileConfig('logging.conf')
 
 def function_call_tracking_decorator(function: Callable[[str], None]):
     def wrapper_accepting_arguments(*args):
+        logger = logging.getLogger(__name__)
         print('Function arguments are:')
         for arg in args:
-            print(arg)
+            logger.info(arg)
         function(*args)
     return wrapper_accepting_arguments
 
-logger = logging.getLogger(__name__)
+#logging.config.dictConfig(loggingDict)
+#logger = logging.getLogger(__name__)
 
-#@function_call_tracking_decorator
-#def testFunction(myMode:str) -> None:
-#    myLogger = logging.getLogger(myMode) 
 
