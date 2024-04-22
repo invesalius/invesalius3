a31fc81e9b24cc0207c48e6313d61fa62957fc8b
Naseeha Salam K <97714994+naseehasalam@users.noreply.github.com>
2024-04-23 04:21:19 +0530

    Fix print parantheses (#715)
    Print function missing parentheses at several places in the file

--- docs/devel/example_closure.py
+++ docs/devel/example_closure.py
@@ -2,20 +2,20 @@
 
 def OuterCount(start):
      counter = [start] # counter is 1-element array
-     print "passei por fora"
+     print("passei por fora")
      def InnerCount():
          counter[0] = counter[0] + 1
-         print "passei por dentro"
+         print("passei por dentro")
          return counter[0]
      return InnerCount
 
-print "Init counter at 5"
+print("Init counter at 5")
 count = OuterCount(5)
-print "\nRun counter 3 times"
-print count()
-print count()
-print count()
+print("\nRun counter 3 times")
+print(count())
+print(count())
+print(count())
 
-print "**********"
+print("**********")
 count = OuterCount(0)
-print count()
+print(count())
