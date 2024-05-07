41a4e95c5239870eea1d2ae0c054c167d389b12d
Victor Souza <victorhos@hotmail.com>
2024-05-08 00:39:57 +0200

    UPD: Use a different dialog behavior for MacOS Show and Hide of Preferences

--- invesalius/gui/preferences.py
+++ invesalius/gui/preferences.py
@@ -618,9 +618,10 @@ class TrackerPage(wx.Panel):
         return True
 
     def OnChooseTracker(self, evt, ctrl):
-        # self.GetParent().Enable(False)
-        # self.HideParent()
-        wx.CallAfter(self.GetParent().Hide)
+        if sys.platform == 'darwin':
+            wx.CallAfter(self.GetParent().Hide)
+        else:
+            self.HideParent()
         Publisher.sendMessage('Begin busy cursor')
         Publisher.sendMessage('Update status text in GUI',
                               label=_("Configuring tracker ..."))
@@ -635,10 +636,11 @@ class TrackerPage(wx.Panel):
         Publisher.sendMessage('Update status text in GUI', label=_("Ready"))
         Publisher.sendMessage("Tracker changed")
         ctrl.SetSelection(self.tracker.tracker_id)
-        # self.ShowParent()
-        # self.GetParent().Enable(True)
         Publisher.sendMessage('End busy cursor')
-        wx.CallAfter(self.GetParent().Show)
+        if sys.platform == 'darwin':
+            wx.CallAfter(self.GetParent().Show)
+        else:
+            self.ShowParent()
 
     def OnChooseReferenceMode(self, evt, ctrl):
         # Probably need to refactor object registration as a whole to use the 
