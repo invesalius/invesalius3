invesalius/invesalius3

commit 8a62bacd2d501aac94a7cfae4d3758c46fd967b7

Request to look into my branch and 'pull' if it is reasonable (#780)
* Added  page for Logging in Preferences

* ...

* updated session.py

* ...

* updated frame.py

* ...

* '...'

* constants.py file updated for proper saving of options chosen

* removed debug print statements

* added logfilr name text field

* Log file name: choosing a default name, saving, loading options rectified

* Changed the logging interface

* Changed the logging interface

* Logging file selection completed.

* Moved logging option types as constants

* Changed RadioButton to Choices window for logging level selection to reduce window width

* Begun log.py file for logging options

* ...

* ...

* some changes for logging. not done yet ...

* some changes for logging. not done yet ...

* ...

* ...

* ...

* ...

* ...

* ...

* ...

* Adding an error catching decorator

* refactoring log preference interface

* Changes to have interface for console and file logging. testing due

* ...

* ...

* Rool Logger setLevel needs to be called after adding all handlers

* moved __set_default_logfile__ from session.py to log.py

* Beginnng to work of directing console log into a wx Text window

* Console logging messages in a separate window - WIP

* Separate window for console stream log. Still WIP.

* Messages still not coming, but no issue with consle window

* Window interface for console logging. Almost there

* some cleaning

* Need to figure log level settings at different levels for console, file, general

* sill WIP

* sill WIP

* bugs in console log window removed, but still work in progress

* Commented out the /close button. Goof fo r now.

* Removed recursive buggy call.

* default log file name option added

* cleanup a bit

* ...

* some clean up

* Replaced calling new logger with invLogger

* Replaced calling new logger with log.invLogger

invesalius/gui/preferences.py
@@ -6,7 +6,8 @@ import nibabel as nb
 import numpy as np
 import invesalius.constants as const
 import invesalius.session as ses
-import invesalius.gui.dialogs as dlg
+import invesalius.gui.log as log
+import invesalius.gui.dialogs as dlg0
 import invesalius.data.vtk_utils as vtk_utils
 from invesalius import inv_paths
 
@@ -36,8 +37,12 @@ class Preferences(wx.Dialog):
 
         self.book = wx.Notebook(self, -1)
 
+        self.have_log_tab = 1
+                                         
         self.visualization_tab = VisualizationTab(self.book)
         self.language_tab = LanguageTab(self.book)
+        if self.have_log_tab==1:
+            self.logging_tab = LoggingTab(self.book)
 
         self.book.AddPage(self.visualization_tab, _("Visualization"))
 
@@ -52,6 +57,7 @@ class Preferences(wx.Dialog):
                 pedal_connector=pedal_connector,
                 neuronavigation_api=neuronavigation_api,
             )
+
             self.navigation_tab = NavigationTab(self.book, navigation)
             self.tracker_tab = TrackerTab(self.book, tracker, robot)
             self.object_tab = ObjectTab(self.book, navigation, tracker, pedal_connector, neuronavigation_api)
@@ -61,6 +67,8 @@ class Preferences(wx.Dialog):
             self.book.AddPage(self.object_tab, _("Stimulator"))
 
         self.book.AddPage(self.language_tab, _("Language"))
+        if self.have_log_tab==1:
+            self.book.AddPage(self.logging_tab, _("Logging"))
 
         btnsizer = self.CreateStdDialogButtonSizer(wx.OK | wx.CANCEL)
         min_width = max([i.GetMinWidth() for i in (self.book.GetChildren())])
@@ -81,20 +89,34 @@ class Preferences(wx.Dialog):
 
     def GetPreferences(self):
         values = {}
+
         lang = self.language_tab.GetSelection()
         viewer = self.visualization_tab.GetSelection()
+
         values.update(lang)
         values.update(viewer)
+
+        if self.have_log_tab==1:
+            logging = self.logging_tab.GetSelection()
+            values.update(logging)
 
         return values
 
     def LoadPreferences(self):
         session = ses.Session()
-
         rendering = session.GetConfig('rendering')
         surface_interpolation = session.GetConfig('surface_interpolation')
         language = session.GetConfig('language')
         slice_interpolation = session.GetConfig('slice_interpolation')
+
+        #logger = log.MyLogger()
+        file_logging = log.invLogger.GetConfig('file_logging')
+        file_logging_level = log.invLogger.GetConfig('file_logging_level')
+        append_log_file = log.invLogger.GetConfig('append_log_file')
+        logging_file  = log.invLogger.GetConfig('logging_file')
+        console_logging = log.invLogger.GetConfig('console_logging')
+        console_logging_level = log.invLogger.GetConfig('console_logging_level')
+
         mode = session.GetConfig('mode')
 
         if mode == const.MODE_NAVIGATOR:
@@ -105,10 +127,18 @@ class Preferences(wx.Dialog):
             const.SURFACE_INTERPOLATION: surface_interpolation,
             const.LANGUAGE: language,
             const.SLICE_INTERPOLATION: slice_interpolation,
+            const.FILE_LOGGING: file_logging,
+            const.FILE_LOGGING_LEVEL: file_logging_level,
+            const.APPEND_LOG_FILE: append_log_file,           
+            const.LOGFILE: logging_file,
+            const.CONSOLE_LOGGING: console_logging,
+            const.CONSOLE_LOGGING_LEVEL: console_logging_level,
         }
 
         self.visualization_tab.LoadSelection(values)
         self.language_tab.LoadSelection(values)
+        if self.have_log_tab==1:
+            self.logging_tab.LoadSelection(values)
 
 
 class VisualizationTab(wx.Panel):
@@ -168,7 +198,6 @@ class VisualizationTab(wx.Panel):
             const.SURFACE_INTERPOLATION: self.rb_inter.GetSelection(),
             const.SLICE_INTERPOLATION: self.rb_inter_sl.GetSelection()
         }
-
         return options
 
     def LoadSelection(self, values):
@@ -177,9 +206,153 @@ class VisualizationTab(wx.Panel):
         slice_interpolation = values[const.SLICE_INTERPOLATION]
 
         self.rb_rendering.SetSelection(int(rendering))
-        self.rb_inter.SetSelection(int(surface_interpolation))
+        self.rb_inter.SetSelection(int(surface_interpolation)) 
         self.rb_inter_sl.SetSelection(int(slice_interpolation))
 
+class LoggingTab(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        
+        #File Logging Selection
+        bsizer_logging = wx.StaticBoxSizer(wx.VERTICAL, self, _("File Logging Options"))
+
+        bsizer_file_logging = wx.BoxSizer(wx.HORIZONTAL) 
+
+        rb_file_logging = self.rb_file_logging = wx.RadioBox(
+            self,
+            -1,
+            label="Do Logging",
+            choices=["No", "Yes"],
+            majorDimension=2,
+            style=wx.RA_SPECIFY_COLS | wx.NO_BORDER | wx.FIXED_MINSIZE,
+        )
+        bsizer_file_logging.Add(rb_file_logging, 0, wx.TOP | wx.LEFT | wx.FIXED_MINSIZE, 5)
+
+        rb_append_file = self.rb_append_file = wx.RadioBox(
+            self, #bsizer_file_logging.GetStaticBox(),
+            -1,
+            label = "Append File",
+            choices=["No", "Yes"],
+            majorDimension=2,
+            style=wx.RA_SPECIFY_COLS | wx.NO_BORDER | wx.FIXED_MINSIZE,
+        )
+
+        lbl_file_logging_level = wx.StaticText(self, -1, _(" Logging Level "))
+        cb_file_logging_level = self.cb_file_logging_level = wx.Choice(
+            self,
+            -1,
+            name="Logging Level",
+            choices=const.LOGGING_LEVEL_TYPES, 
+        ) 
+        bsizer_file_logging.Add(rb_append_file, 0, wx.TOP | wx.LEFT | wx.FIXED_MINSIZE, 5)
+        bsizer_file_logging.Add(lbl_file_logging_level, 0, wx.TOP | wx.LEFT | wx.FIXED_MINSIZE , 5)
+        bsizer_file_logging.Add(cb_file_logging_level, 0, wx.TOP | wx.LEFT | wx.FIXED_MINSIZE, 5)
+
+        bsizer_logging.Add(bsizer_file_logging, 0, wx.TOP | wx.LEFT | wx.EXPAND , 0)
+
+        bsizer_log_filename = wx.BoxSizer(wx.HORIZONTAL) 
+
+        lbl_log_file_label = wx.StaticText(self, -1, _("File:"))
+        tc_log_file_name = self.tc_log_file_name = wx.TextCtrl(
+            self, -1, "", 
+            style = wx.TE_READONLY | wx.TE_LEFT,  
+            size=(300, -1))
+        tc_log_file_name.SetForegroundColour(wx.BLUE)
+        bt_log_file_select = wx.Button(self, label="Modify") #bsizer_file_logging.GetStaticBox()
+        bt_log_file_select.Bind(wx.EVT_BUTTON, self.OnModifyButton)
+        bsizer_log_filename.Add(lbl_log_file_label, 0, wx.TOP | wx.LEFT , 0) #| wx.FIXED_MINSIZE, 0)
+        bsizer_log_filename.Add(tc_log_file_name, 0, wx.TOP | wx.LEFT , 0) #| wx.FIXED_MINSIZE, 0)
+        bsizer_log_filename.Add(bt_log_file_select, 0, wx.TOP | wx.LEFT , 0) #| wx.FIXED_MINSIZE, 0)
+        bsizer_logging.Add(bsizer_log_filename, 0, wx.TOP | wx.LEFT | wx.FIXED_MINSIZE, 0)
+
+        #Console Logging Selection
+        bsizer_console_logging = wx.StaticBoxSizer(wx.HORIZONTAL, self, _(" Console Logging Options"))
+        
+        rb_console_logging = self.rb_console_logging = wx.RadioBox(
+            bsizer_console_logging.GetStaticBox(),
+            -1,
+            label = "Do logging",
+            choices=["No", "Yes"],
+            majorDimension=2,
+            style=wx.RA_SPECIFY_COLS | wx.NO_BORDER | wx.FIXED_MINSIZE,
+        )
+        bsizer_console_logging.Add(rb_console_logging, 0, wx.TOP | wx.LEFT | wx.FIXED_MINSIZE, 5)
+        lbl_console_logging_level = wx.StaticText(bsizer_console_logging.GetStaticBox(), -1, _(" Logging Level "))
+        cb_console_logging_level = self.cb_console_logging_level = wx.Choice(
+            bsizer_console_logging.GetStaticBox(),
+            -1,
+            choices=const.LOGGING_LEVEL_TYPES, 
+        ) 
+        bsizer_console_logging.Add(lbl_console_logging_level, 0, wx.TOP | wx.LEFT | wx.FIXED_MINSIZE, 5)
+        bsizer_console_logging.Add(cb_console_logging_level, 0, wx.TOP | wx.LEFT | wx.FIXED_MINSIZE, 5)
+
+        border = wx.BoxSizer(wx.VERTICAL)
+        border.Add(bsizer_logging, 1, wx.EXPAND | wx.ALL , 10) #| wx.FIXED_MINSIZE, 10)
+        border.Add(bsizer_console_logging, 1, wx.EXPAND | wx.ALL , 10) #| wx.FIXED_MINSIZE, 10)
+        self.SetSizerAndFit(border)
+
+        self.Layout()
+
+    @log.call_tracking_decorator
+    def OnModifyButton(self,e):
+        logging_file = self.tc_log_file_name.GetValue()
+        path, fname = os.path.split(logging_file)
+        dlg = wx.FileDialog(self, message = "Save Log Contents",
+            defaultDir = path, #os.getcwd(),
+            defaultFile = fname, #default_file,
+            wildcard = "Log files (*.log)|*.log",
+            style = wx.FD_SAVE|wx.FD_OVERWRITE_PROMPT)
+        if dlg.ShowModal() == wx.ID_CANCEL:
+            dlg.Destroy()
+            return False
+
+        file_path = dlg.GetPath()
+        self.tc_log_file_name.SetValue(file_path)
+        dlg.Destroy()
+        return True
+
+    def GetSelection(self):
+        options = {
+            const.FILE_LOGGING: self.rb_file_logging.GetSelection(),
+            const.FILE_LOGGING_LEVEL: self.cb_file_logging_level.GetSelection(),
+            const.APPEND_LOG_FILE: self.rb_append_file.GetSelection(),
+            const.LOGFILE: self.tc_log_file_name.GetValue(),
+            const.CONSOLE_LOGGING: self.rb_console_logging.GetSelection(),
+            const.CONSOLE_LOGGING_LEVEL: self.cb_console_logging_level.GetSelection(),
+        }
+        #session = ses.Session()
+        #logger = log.MyLogger()
+    
+        file_logging = self.rb_file_logging.GetSelection()
+        log.invLogger.SetConfig('file_logging', file_logging)
+        file_logging_level = self.cb_file_logging_level.GetSelection()
+        log.invLogger.SetConfig('file_logging_level', file_logging_level)
+        append_log_file = self.rb_append_file.GetSelection()
+        log.invLogger.SetConfig('append_log_file', append_log_file)
+        logging_file  = self.tc_log_file_name.GetValue()
+        log.invLogger.SetConfig('logging_file', logging_file)
+        console_logging = self.rb_console_logging.GetSelection()
+        log.invLogger.SetConfig('console_logging', console_logging)
+        console_logging_level = self.cb_console_logging_level.GetSelection()
+        log.invLogger.SetConfig('console_logging_level', console_logging_level)
+        log.invLogger.configureLogging()
+
+        return options
+
+    def LoadSelection(self, values):
+        file_logging = values[const.FILE_LOGGING]
+        file_logging_level = values[const.FILE_LOGGING_LEVEL]
+        append_log_file = values[const.APPEND_LOG_FILE]
+        logging_file = values[const.LOGFILE]
+        console_logging = values[const.CONSOLE_LOGGING]
+        console_logging_level = values[const.CONSOLE_LOGGING_LEVEL]
+       
+        self.rb_file_logging.SetSelection(int(file_logging))
+        self.cb_file_logging_level.SetSelection(int(file_logging_level))
+        self.rb_append_file.SetSelection(int(append_log_file))
+        self.tc_log_file_name.SetValue(logging_file)
+        self.rb_console_logging.SetSelection(int(console_logging))
+        self.cb_console_logging_level.SetSelection(int(console_logging_level))
 
 class NavigationTab(wx.Panel):
     def __init__(self, parent, navigation):

app.py
@@ -99,6 +99,7 @@ if session.ReadConfig():
         except FileNotFoundError:
             pass
 
+import invesalius.gui.log as log
 
 class InVesalius(wx.App):
     """
@@ -131,6 +132,8 @@ class InVesalius(wx.App):
         self.SetTopWindow(self.frame)
         self.frame.Show()
         self.frame.Raise()
+        #logger = log.MyLogger()
+        log.invLogger.configureLogging()
 
 # ------------------------------------------------------------------
 

invesalius/constants.py
@@ -647,6 +647,15 @@ SURFACE_INTERPOLATION = 1
 LANGUAGE = 2
 SLICE_INTERPOLATION = 3
 
+#------------ Logging options key------------
+FILE_LOGGING = 4
+FILE_LOGGING_LEVEL = 5
+APPEND_LOG_FILE = 6
+LOGFILE = 7
+CONSOLE_LOGGING = 8
+CONSOLE_LOGGING_LEVEL = 9
+LOGGING_LEVEL_TYPES = ['NOTSET', 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL']
+
 #Correlaction extracted from pyDicom
 DICOM_ENCODING_TO_PYTHON = {
                             'None':'iso8859',

invesalius/gui/frame.py
@@ -36,6 +36,7 @@ import invesalius.gui.preferences as preferences
 import invesalius.project as prj
 import invesalius.session as ses
 import invesalius.utils as utils
+import invesalius.gui.log as log
 import wx
 import wx.aui
 import wx.lib.popupctl as pc
@@ -136,6 +137,7 @@ class Frame(wx.Frame):
         self.__bind_events()
         self.__bind_events_wx()
 
+        #log.initLogger()
 
     def __bind_events(self):
         """
@@ -449,9 +451,13 @@ class Frame(wx.Frame):
         save = dialog.IsCheckBoxChecked()
         dialog.Destroy()
 
+        #logger = log.MyLogger()
+
         if not save and answer == wx.ID_YES:
+            log.invLogger.closeLogging()
             return 1  # Exit and delete session
         elif save and answer == wx.ID_YES:
+            log.invLogger.closeLogging()
             return 2  # Exit without deleting session
         else:
             return 0  # Don't Exit
@@ -674,6 +680,7 @@ class Frame(wx.Frame):
 
         if preferences_dialog.ShowModal() == wx.ID_OK:
             values = preferences_dialog.GetPreferences()
+ 
             preferences_dialog.Destroy()
 
             session = ses.Session()
@@ -682,11 +689,23 @@ class Frame(wx.Frame):
             surface_interpolation = values[const.SURFACE_INTERPOLATION]
             language = values[const.LANGUAGE]
             slice_interpolation = values[const.SLICE_INTERPOLATION]
-
+            file_logging  = values[const.FILE_LOGGING]
+            file_logging_level  = values[const.FILE_LOGGING_LEVEL]
+            append_log_file  = values[const.APPEND_LOG_FILE]
+            logging_file = values[const.LOGFILE]
+            console_logging  = values[const.CONSOLE_LOGGING]
+            console_logging_level  = values[const.CONSOLE_LOGGING_LEVEL]
+            
             session.SetConfig('rendering', rendering)
             session.SetConfig('surface_interpolation', surface_interpolation)
             session.SetConfig('language', language)
             session.SetConfig('slice_interpolation', slice_interpolation)
+            session.SetConfig('file_logging', file_logging)
+            session.SetConfig('file_logging_level', file_logging_level)
+            session.SetConfig('append_log_file', append_log_file)
+            session.SetConfig('logging_file', logging_file)
+            session.SetConfig('console_logging', console_logging)
+            session.SetConfig('console_logging_level', console_logging_level)
 
             Publisher.sendMessage('Remove Volume')
             Publisher.sendMessage('Reset Raycasting')
@@ -694,6 +713,7 @@ class Frame(wx.Frame):
             Publisher.sendMessage('Update Slice Interpolation MenuBar')
             Publisher.sendMessage('Update Navigation Mode MenuBar')
             Publisher.sendMessage('Update Surface Interpolation')
+
 
     def ShowAbout(self):
         """

commit 473496a840f8128e8331511b8aeadde75ecef760

Fix app-breaking typo introduced in commit 8a62bacd2d501aac94a7cfae4d3758c46fd967b7

invesalius/gui/preferences.py
@@ -7,7 +7,7 @@ import numpy as np
 import invesalius.constants as const
 import invesalius.session as ses
 import invesalius.gui.log as log
-import invesalius.gui.dialogs as dlg0
+import invesalius.gui.dialogs as dlg
 import invesalius.data.vtk_utils as vtk_utils
 from invesalius import inv_paths
 