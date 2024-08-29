6c6215f006dd6612ffb20b86a386c7d9333b58a9
Miika Vuorio <miika.vuorio@aalto.fi>
2024-08-29 17:00:52 +0300

    slightly different numbers

--- invesalius/data/viewer_volume.py
+++ invesalius/data/viewer_volume.py
@@ -1170,12 +1170,12 @@ class Viewer(wx.Panel):
             print("creating force_compensate_arrow_up")
             # force_compensate_arrow_up = self.actor_factory.CreateArrow([0, 0, 0], [0, 0, 50])
             force_compensate_arrow_up = self.actor_factory.CreateArrow(
-                [0, 0, 0], [0, 50, 0]
+                [0, 0, 0], [0, -50, 0]
             )
-            force_compensate_arrow_up.SetPosition(0, -600, 0)
+            force_compensate_arrow_up.SetPosition(0, -550, 0)
             force_compensate_arrow_up.RotateY(90)
             force_compensate_arrow_up.RotateZ(180)
-            force_compensate_arrow_up.GetProperty().SetColor(0, 1, 0)
+            force_compensate_arrow_up.GetProperty().SetColor(1, 1, 0)
 
 
 
