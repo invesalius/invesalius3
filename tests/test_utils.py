5b615c457eb4a879af76fd859a8fbcdbbb5640d2
OfficerK <hossam.sabeer55@gmail.com>
2025-04-21 02:53:20 +0200

    Add test for Singleton pattern with/without args

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -25,3 +25,24 @@ from invesalius.utils import (
     touch,
     vtkarray_to_numpy,
 )
+
+
+class TestSingleton(unittest.TestCase):
+    def test_singleton_pattern(self):
+        class SingletonTest(metaclass=Singleton):
+            pass
+
+        instance1 = SingletonTest()
+        instance2 = SingletonTest()
+        self.assertIs(instance1, instance2)
+
+    def test_singleton_with_args(self):
+        class SingletonWithArgs(metaclass=Singleton):
+            def __init__(self, value=None):
+                self.value = value
+
+        instance1 = SingletonWithArgs("test")
+        instance2 = SingletonWithArgs("different")
+        self.assertIs(instance1, instance2)
+        self.assertEqual(instance1.value, "test")
+        self.assertEqual(instance2.value, "test")
