03ccb1fb51f44c6df1582c4e22adcb0b04cb79ed
Mann Patwa <mannpatwa51@gmail.com>
2025-04-28 20:42:41 +0530

    install tinygrad from pypi(not from source)

--- invesalius/__version__.py
+++ invesalius/__version__.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev4130+ge98c274'
-__version_tuple__ = version_tuple = (0, 1, 'dev4130', 'ge98c274')
+__version__ = version = '0.1.dev4131+gc9a5529.d20250428'
+__version_tuple__ = version_tuple = (0, 1, 'dev4131', 'gc9a5529.d20250428')
