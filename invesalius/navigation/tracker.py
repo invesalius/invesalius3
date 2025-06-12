58ae6de9721e78621f4b295f908e7fe10024e11e
MarcioCamposJr <marcio.campos6@usp.br>
2025-06-11 21:46:19 -0300

    ADD: Reset coil registrations after connect in tracker to avoid bug

--- invesalius/navigation/tracker.py
+++ invesalius/navigation/tracker.py
@@ -147,6 +147,8 @@ class Tracker(metaclass=Singleton):
                 if self.thread_coord is not None:
                     self.thread_coord.start()
 
+                self.ResetCoilRegistrations()
+
             self.SaveState()
 
     def DisconnectTracker(self) -> None:
@@ -280,3 +282,7 @@ class Tracker(metaclass=Singleton):
 
     def get_trackers(self) -> List[str]:
         return cast(List[str], const.TRACKERS)
+
+    def ResetCoilRegistrations(self) -> None:
+        session = ses.Session()
+        session.SetConfig("coil_registrations", {})
