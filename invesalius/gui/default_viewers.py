3e476224ef48c3f1bf4cca9fc1a44113f82f40f3
Felipe Cybis Pereira <felipe.cybispereira@gmail.com>
2025-07-10 16:52:30 +0200

    feat: add pubsub message to update the caption of a panel in the default viewers

--- invesalius/gui/default_viewers.py
+++ invesalius/gui/default_viewers.py
@@ -20,6 +20,7 @@ import os
 import sys
 
 import wx
+import wx.aui
 import wx.lib.agw.fourwaysplitter as fws
 import wx.lib.colourselect as csel
 import wx.lib.platebtn as pbtn
@@ -160,7 +161,12 @@ class Panel(wx.Panel):
     def __bind_events(self):
         Publisher.subscribe(self.OnSetTargetMode, "Set target mode")
         Publisher.subscribe(self.OnStartNavigation, "Start navigation")
+        Publisher.subscribe(self.UpdateViewerCaption, "Update viewer caption")
         Publisher.subscribe(self._Exit, "Exit")
+
+    def UpdateViewerCaption(self, viewer_name: str, caption: str):
+        self.aui_manager.GetPane(viewer_name).Caption(caption)
+        self.Refresh()
 
     def OnSetTargetMode(self, enabled=True):
         if enabled:
