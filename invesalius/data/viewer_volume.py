ee69d17cbe4eba391d620c0f44b5aa50c9e07b5a
Marcio Campos <82606834+MarcioCamposJr@users.noreply.github.com>
2025-08-18 17:16:17 -0300

    ADD: Dual navigation views in multitarget mode

--- invesalius/data/viewer_volume.py
+++ invesalius/data/viewer_volume.py
@@ -123,7 +123,7 @@ PROP_MEASURE = 0.8
 
 
 class Viewer(wx.Panel):
-    def __init__(self, parent):
+    def __init__(self, parent, showSensors):
         display_size = wx.GetDisplaySize()
         # Set the initial volume wx.Panel size as half the screen resolution to fix the issue
         # with small target guide icons when loading a state file with target selected
@@ -139,6 +139,7 @@ class Viewer(wx.Panel):
         self.static_markers_efield = []
         self.plot_vector = None
         self.style = None
+        self.showSensors = showSensors
 
         interactor = wxVTKRenderWindowInteractor(self, -1, size=self.GetSize())
         self.interactor = interactor
@@ -275,6 +276,7 @@ class Viewer(wx.Panel):
         self.dummy_ref_actor = None
         self.dummy_obj_actor = None
         self.coil_sensor_spheres = []
+        self.coil_sensor_sources = []
         self.target_mode = False
 
         # Set the angle and distance thresholds.
@@ -461,7 +463,9 @@ class Viewer(wx.Panel):
 
         Publisher.subscribe(self.RemoveVolume, "Remove Volume")
 
-        Publisher.subscribe(self.OnSensors, "Sensors ID")
+        if self.showSensors:
+            Publisher.subscribe(self.OnSensors, "Sensors ID")
+
         Publisher.subscribe(self.OnRemoveSensorsID, "Remove sensors ID")
 
         # TODO: This shouldn't be here, rather in marker_visualizer.py. The problem is that
@@ -537,6 +541,197 @@ class Viewer(wx.Panel):
         Publisher.subscribe(
             self.EnableSaveAutomaticallyEfieldData, "Save automatically efield data"
         )
+        Publisher.subscribe(self.UpdateSensorIDSize, "Update size sensors")
+
+    def Cleanup(self):
+        # Unsubscribe all pubsub listeners
+        Publisher.unsubscribe(self.AddSurface, "Load surface actor into viewer")
+        Publisher.unsubscribe(self.RemoveSurface, "Remove surface actor from viewer")
+        Publisher.unsubscribe(self.UpdateRender, "Render volume viewer")
+        Publisher.unsubscribe(self.ChangeBackgroundColour, "Change volume viewer background colour")
+        Publisher.unsubscribe(self.LoadVolume, "Load volume into viewer")
+        Publisher.unsubscribe(self.UnloadVolume, "Unload volume")
+        Publisher.unsubscribe(self.OnSetWindowLevelText, "Set volume window and level text")
+        Publisher.unsubscribe(self.OnHideRaycasting, "Hide raycasting volume")
+        Publisher.unsubscribe(self.OnShowRaycasting, "Update raycasting preset")
+        Publisher.unsubscribe(self.AppendActor, "AppendActor")
+        Publisher.unsubscribe(self.SetWidgetInteractor, "Set Widget Interactor")
+        Publisher.unsubscribe(self.OnSetViewAngle, "Set volume view angle")
+        Publisher.unsubscribe(
+            self.OnDisableBrightContrast, "Set interaction mode " + str(const.MODE_SLICE_EDITOR)
+        )
+        Publisher.unsubscribe(self.OnExportSurface, "Export surface to file")
+        Publisher.unsubscribe(self.LoadSlicePlane, "Load slice plane")
+        Publisher.unsubscribe(self.ResetCamClippingRange, "Reset cam clipping range")
+        Publisher.unsubscribe(self.enable_style, "Enable style")
+        Publisher.unsubscribe(self.OnDisableStyle, "Disable style")
+        Publisher.unsubscribe(self.OnHideText, "Hide text actors on viewers")
+        Publisher.unsubscribe(self.AddActors, "Add actors " + str(const.SURFACE))
+        Publisher.unsubscribe(self.RemoveActors, "Remove actors " + str(const.SURFACE))
+        Publisher.unsubscribe(self.OnShowText, "Show text actors on viewers")
+        Publisher.unsubscribe(self.OnShowRuler, "Show rulers on viewers")
+        Publisher.unsubscribe(self.OnHideRuler, "Hide rulers on viewers")
+        Publisher.unsubscribe(self.OnRulerVisibilityStatus, "Receive ruler visibility status")
+        Publisher.unsubscribe(self.OnCloseProject, "Close project data")
+        Publisher.unsubscribe(self.RemoveAllActors, "Remove all volume actors")
+        Publisher.unsubscribe(self.OnExportPicture, "Export picture to file")
+        Publisher.unsubscribe(self.OnStartSeed, "Create surface by seeding - start")
+        Publisher.unsubscribe(self.OnEndSeed, "Create surface by seeding - end")
+        Publisher.unsubscribe(self.SetStereoMode, "Set stereo mode")
+        Publisher.unsubscribe(self.Reposition3DPlane, "Reposition 3D Plane")
+        Publisher.unsubscribe(self.UpdatePointer, "Update volume viewer pointer")
+        Publisher.unsubscribe(self.RemoveVolume, "Remove Volume")
+        Publisher.unsubscribe(self.OnSensors, "Sensors ID")
+        Publisher.unsubscribe(self.OnRemoveSensorsID, "Remove sensors ID")
+        Publisher.unsubscribe(self.DeleteEFieldMarkers, "Delete markers")
+        Publisher.unsubscribe(self.OnNavigationStatus, "Navigation status")
+        Publisher.unsubscribe(self.UpdateArrowPose, "Update object arrow matrix")
+        Publisher.unsubscribe(
+            self.UpdateEfieldPointLocation, "Update point location for e-field calculation"
+        )
+        Publisher.unsubscribe(self.GetEnorm, "Get enorm")
+        Publisher.unsubscribe(self.TrackObject, "Track object")
+        Publisher.unsubscribe(self.SetTargetMode, "Set target mode")
+        Publisher.unsubscribe(self.OnUpdateCoilPose, "Update coil pose")
+        Publisher.unsubscribe(self.OnSetTarget, "Set target")
+        Publisher.unsubscribe(self.OnUnsetTarget, "Unset target")
+        Publisher.unsubscribe(self.OnUpdateAngleThreshold, "Update angle threshold")
+        Publisher.unsubscribe(self.OnUpdateDistanceThreshold, "Update distance threshold")
+        Publisher.unsubscribe(self.OnUpdateTracts, "Update tracts")
+        Publisher.unsubscribe(self.OnUpdateEfieldvis, "Update efield vis")
+        Publisher.unsubscribe(self.InitializeColorArray, "Initialize color array")
+        Publisher.unsubscribe(self.OnRemoveTracts, "Remove tracts")
+        Publisher.unsubscribe(self.UpdateSeedOffset, "Update seed offset")
+        Publisher.unsubscribe(self.UpdateMarkerOffsetState, "Update marker offset state")
+        Publisher.unsubscribe(self.AddPeeledSurface, "Update peel")
+        Publisher.unsubscribe(self.InitEfield, "Initialize E-field brain")
+        Publisher.unsubscribe(self.GetPeelCenters, "Get peel centers and normals")
+        Publisher.unsubscribe(self.InitLocatorViewer, "Get init locator")
+        Publisher.unsubscribe(self.GetPeelCenters, "Get peel centers and normals")
+        Publisher.unsubscribe(self.InitLocatorViewer, "Get init locator")
+        Publisher.unsubscribe(self.load_mask_preview, "Load mask preview")
+        Publisher.unsubscribe(self.remove_mask_preview, "Remove mask preview")
+        Publisher.unsubscribe(self.GetEfieldActor, "Send Actor")
+        Publisher.unsubscribe(self.ReturnToDefaultColorActor, "Recolor again")
+        Publisher.unsubscribe(self.SaveEfieldData, "Save Efield data")
+        Publisher.unsubscribe(self.SavedAllEfieldData, "Save all Efield data")
+        Publisher.unsubscribe(self.SaveEfieldTargetData, "Save target data")
+        Publisher.unsubscribe(self.GetTargetSavedEfieldData, "Get target index efield")
+        Publisher.unsubscribe(self.CheckStatusSavedEfieldData, "Check efield data")
+        Publisher.unsubscribe(self.GetNeuronavigationApi, "Get Neuronavigation Api")
+        Publisher.unsubscribe(self.UpdateEfieldPointLocationOffline, "Update interseccion offline")
+        Publisher.unsubscribe(self.MaxEfieldActor, "Show max Efield actor")
+        Publisher.unsubscribe(self.CoGEfieldActor, "Show CoG Efield actor")
+        Publisher.unsubscribe(
+            self.CalculateDistanceMaxEfieldCoGE, "Show distance between Max and CoG Efield"
+        )
+        Publisher.unsubscribe(self.EfieldVectors, "Show Efield vectors")
+        Publisher.unsubscribe(self.RecolorEfieldActor, "Recolor efield actor")
+        Publisher.unsubscribe(self.GetScalpEfield, "Send scalp index")
+        Publisher.unsubscribe(
+            self.OnUpdateRobotWarning, "Robot to Neuronavigation: Update robot warning"
+        )
+        Publisher.unsubscribe(self.GetCoilPosition, "Calculate position and rotation")
+        Publisher.unsubscribe(
+            self.CreateCortexProjectionOnScalp, "Send efield target position on brain"
+        )
+        Publisher.unsubscribe(self.UpdateEfieldThreshold, "Update Efield Threshold")
+        Publisher.unsubscribe(self.UpdateEfieldROISize, "Update Efield ROI size")
+        Publisher.unsubscribe(self.SetEfieldTargetAtCortex, "Set as Efield target at cortex")
+        Publisher.unsubscribe(self.EnableShowEfieldAboveThreshold, "Show area above threshold")
+        Publisher.unsubscribe(self.EnableEfieldTools, "Enable Efield tools")
+        Publisher.unsubscribe(self.ClearTargetAtCortex, "Clear efield target at cortex")
+        Publisher.unsubscribe(self.CoGEforCortexMarker, "Get Cortex position")
+        Publisher.unsubscribe(self.AddCortexMarkerActor, "Add cortex marker actor")
+        Publisher.unsubscribe(self.CortexMarkersVisualization, "Display efield markers at cortex")
+        Publisher.unsubscribe(self.GetTargetPositions, "Get targets Ids for mtms")
+        Publisher.unsubscribe(self.GetTargetPathmTMS, "Send targeting file path")
+        Publisher.unsubscribe(self.GetdIsfromCoord, "Send mtms coords")
+        Publisher.unsubscribe(
+            self.EnableSaveAutomaticallyEfieldData, "Save automatically efield data"
+        )
+
+        # Clean up VTK objects
+        if self.interactor:
+            self.interactor.RemoveAllObservers()
+            self.interactor.SetRenderWindow(None)
+            self.interactor.Enable(0)
+            self.interactor.Destroy()
+            self.interactor = None
+
+        if self.ren:
+            self.ren.RemoveAllViewProps()
+            self.ren.SetRenderWindow(None)
+            self.ren = None
+
+        if self.target_guide_renderer:
+            self.target_guide_renderer.RemoveAllViewProps()
+            self.target_guide_renderer.SetRenderWindow(None)
+            self.target_guide_renderer = None
+
+        if self.canvas_renderer:
+            self.canvas_renderer.RemoveAllViewProps()
+            self.canvas_renderer.SetRenderWindow(None)
+            self.canvas_renderer = None
+
+        # Clean up other VTK objects if they exist
+        if self.pointer_actor:
+            self.pointer_actor = None
+        if self.distance_text:
+            self.distance_text = None
+        if self.robot_warnings_text:
+            self.robot_warnings_text = None
+        if self.ruler:
+            self.ruler = None
+        if self.slice_plane:
+            self.slice_plane = None
+        if self.actor_peel:
+            self.actor_peel = None
+        if self.object_orientation_torus_actor:
+            self.object_orientation_torus_actor = None
+        if self.obj_projection_arrow_actor:
+            self.obj_projection_arrow_actor = None
+        if self.vectorfield_actor:
+            self.vectorfield_actor = None
+        if self.efield_scalar_bar:
+            self.efield_scalar_bar = None
+        if self.edge_actor:
+            self.edge_actor = None
+        if self.SpreadEfieldFactorTextActor:
+            self.SpreadEfieldFactorTextActor = None
+        if self.mTMSCoordTextActor:
+            self.mTMSCoordTextActor = None
+        if self.ClusterEfieldTextActor:
+            self.ClusterEfieldTextActor = None
+        if self.EfieldAtTargetLegend:
+            self.EfieldAtTargetLegend = None
+        if self.max_efield_vector:
+            self.max_efield_vector = None
+        if self.ball_max_vector:
+            self.ball_max_vector = None
+        if self.GoGEfieldVector:
+            self.GoGEfieldVector = None
+        if self.ball_GoGEfieldVector:
+            self.ball_GoGEfieldVector = None
+        if self.dummy_probe_actor:
+            self.dummy_probe_actor = None
+        if self.dummy_ref_actor:
+            self.dummy_ref_actor = None
+        if self.dummy_obj_actor:
+            self.dummy_obj_actor = None
+        for actor in self.coil_sensor_spheres:
+            actor = None
+        self.coil_sensor_spheres = []
+        for actor, _ in self.static_markers_efield:
+            actor = None
+        self.static_markers_efield = []
+
+        # Call UnInit on the interactor's render window
+        if self.interactor and self.interactor.GetRenderWindow():
+            self.interactor.GetRenderWindow().Finalize()
+
+        # Call the base class's cleanup if it exists
+        # super().Cleanup()
 
     def get_vtk_mouse_position(self):
         """
@@ -656,12 +851,16 @@ class Viewer(wx.Panel):
 
         reader = vtkSTLReader()
         reader.SetFileName(filename)
+        reader.Update()
         mapper = vtkPolyDataMapper()
-        mapper.SetInputConnection(reader.GetOutputPort())
+        polydata = reader.GetOutput()
+        center = polydata.GetCenter()
+        mapper.SetInputData(polydata)
         mapper.SetScalarVisibility(0)
 
         dummy_probe_actor = vtkActor()
         dummy_probe_actor.SetMapper(mapper)
+        dummy_probe_actor.SetOrigin(center)
         dummy_probe_actor.GetProperty().SetColor(1, 1, 1)
         dummy_probe_actor.GetProperty().SetOpacity(1.0)
         self.dummy_probe_actor = dummy_probe_actor
@@ -678,12 +877,16 @@ class Viewer(wx.Panel):
 
         reader = vtkSTLReader()
         reader.SetFileName(filename)
+        reader.Update()
         mapper = vtkPolyDataMapper()
-        mapper.SetInputConnection(reader.GetOutputPort())
+        polydata = reader.GetOutput()
+        center = polydata.GetCenter()
+        mapper.SetInputData(polydata)
         mapper.SetScalarVisibility(0)
 
         dummy_ref_actor = vtkActor()
         dummy_ref_actor.SetMapper(mapper)
+        dummy_ref_actor.SetOrigin(center)
         dummy_ref_actor.GetProperty().SetColor(1, 1, 1)
         dummy_ref_actor.GetProperty().SetOpacity(1.0)
         self.dummy_ref_actor = dummy_ref_actor
@@ -700,12 +903,16 @@ class Viewer(wx.Panel):
 
         reader = vtkSTLReader()
         reader.SetFileName(filename)
+        reader.Update()
         mapper = vtkPolyDataMapper()
-        mapper.SetInputConnection(reader.GetOutputPort())
+        polydata = reader.GetOutput()
+        center = polydata.GetCenter()
+        mapper.SetInputData(polydata)
         mapper.SetScalarVisibility(0)
 
         dummy_obj_actor = vtkActor()
         dummy_obj_actor.SetMapper(mapper)
+        dummy_obj_actor.SetOrigin(center)
         dummy_obj_actor.GetProperty().SetColor(1, 1, 1)
         dummy_obj_actor.GetProperty().SetOpacity(1.0)
         self.dummy_obj_actor = dummy_obj_actor
@@ -721,6 +928,7 @@ class Viewer(wx.Panel):
             sphere_source = vtkSphereSource()
             sphere_source.SetRadius(6)
             sphere_source.SetCenter(start_x + i * 15, -40, 0)  # Positioned below the coil icon
+            self.coil_sensor_sources.append(sphere_source)
 
             mapper = vtkPolyDataMapper()
             mapper.SetInputConnection(sphere_source.GetOutputPort())
@@ -743,6 +951,33 @@ class Viewer(wx.Panel):
             self.interactor.GetRenderWindow().RemoveRenderer(self.ren_obj)
             self.probe = self.ref = self.obj = False
             self.UpdateRender()
+
+    def UpdateSensorIDSize(self, scale_factor):
+        if not self.probe:
+            return
+
+        print(scale_factor)
+
+        if self.dummy_probe_actor:
+            self.dummy_probe_actor.SetScale(scale_factor, scale_factor, scale_factor)
+        if self.dummy_ref_actor:
+            self.dummy_ref_actor.SetScale(scale_factor, scale_factor, scale_factor)
+        if self.dummy_obj_actor:
+            self.dummy_obj_actor.SetScale(scale_factor, scale_factor, scale_factor)
+
+        base_radius = 6
+        new_radius = base_radius * scale_factor
+        for source in self.coil_sensor_sources:
+            source.SetRadius(new_radius)
+            source.Update()
+
+        if self.ren_probe:
+            self.ren_probe.ResetCamera()
+        if self.ren_ref:
+            self.ren_ref.ResetCamera()
+        if self.ren_obj:
+            self.ren_obj.ResetCamera()
+        self.UpdateRender()
 
     # def OnShowSurface(self, index, visibility):
     #     if visibility:
@@ -2549,6 +2784,7 @@ class Viewer(wx.Panel):
     def __bind_events_wx(self):
         # self.Bind(wx.EVT_SIZE, self.OnSize)
         #  self.canvas.subscribe_event('LeftButtonPressEvent', self.on_insert_point)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
         pass
 
     def on_insert_point(self, evt):
