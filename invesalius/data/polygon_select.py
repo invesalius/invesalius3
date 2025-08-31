05da980d3efa947cff3f2a6018487620c01f183e
Felipe Cybis Pereira <41338087+FelipeCybis@users.noreply.github.com>
2025-09-01 01:41:57 +0200

    Allow edit mask from 3D viewer through polygon projection (#1006)
    * FIX: added pytorch to environment.yml
    
    The absence of the channel was causing pytorch not to be found on
    a windows installation (windows 10 22H2, miniconda3, conda 24.4.0)
    
    * FEAT: Mask 3D edit buttons in task_slice
    
    task_slice.py:
    - Checkbox for enabling the 3D mask edit
    - Button for performing the edit
    - Combobox for choosing the edit mode
    
    constants.py:
    - Style ID for mask edit
    - Operation constants and mode strings (not localized yet)
    
    * FEAT: 3D edit interaction style; polygon select
    
    UI modifications to allow drawing a polygon in the volume renderer
    
    - Canvas Handler for simple polygon selection and rendering;
    - Interaction style to create this polygon and communicate to mask edit.
    
    * FEAT: Implemented frontal 3D editing (no rotation)
    
    Functionality for using the polygon drawn in the volume viewer to cut
    the mask has been implemented.
    
    * FEAT: Multi polygon in 3D mask edit
    
    The user can now select multiple polygons in the volume renderer to
    perform the edit.
    
    * FEAT: UI for depth 3D cut + clear polygons
    
    Added controls in task_slice for toggling the "3D mask cut with depth"
    functionality.
    Added the button for cleaning up the polygons in the volume viewer.
    
    * FEAT: Depth 3D edit implemented
    
    Implemented with a spin control from 0 to 1.
    
    Function parameter improvements in mask_3d_edit.py
    
    * FEAT: save_history (undo) in mask_3d_edit
    
    * FIX: fixed 3d mask cut when volume is rotated
    
    Fixed the problem by inverting the Y axis
    
    * fix: add mask_cut to CMakeList
    
    * chore(typing): add wx.CommandEvent type to some evt param
    
    * feat: implement interactive shell
    
    * fix: add missing 'was_edited' flag to allow selecting surface processing algo
    
    * chore: harmonize name of functions and words on GUI
    
    * chore: improve documentation on mask editing in 3D
    
    * fix: clean up wx binding events when changing 3D styles
    
    * refactor: init polygon in a separate function
    
    * refactor: use get_vtk_mouse_position as it is easier to get type hints
    
    * refactor: remove unnecessary render
    
    * refactor: harmonize and document cleanup and setup functions for mask3d editing style
    
    * chore: document missing functions in mask3d editing style
    
    * feat: add pubsub message to update the caption of a panel in the default viewers
    
    * fix: get information from camera everytime an edit will be made to always be up to date
    
    * fix: remove unused variable
    
    * refactor: simplify filter mask building from polygon
    
    * fix: control polygonselector from mask editor and not from style to be up to date with polygon points
    
    * feat: add volume viewer to interactive shell
    
    * refactor: move mask_3d_edit file under invesalius/data
    
    * chore: ruff
    
    * refactor: `sub = Publisher.subscribe` for visibility
    
    * wip: removing middle layer for 3d masker
    
    * feat: cut mask when dragging event of the polygon ends
    
    * refactor: optimize cython function to check only true mask indices
    
    * fix pre-commit (this will probably be deleted)
    
    * fix: fix when first polygon class is called
    
    * refactor: when no cut is done and polygons are cleared, initial mask comes back
    
    * refactor: small performance improvement with no gil
    
    * refactor: simplify cython function into only one
    
    * refactor: if 3D preview was not enable, disable when leaving style
    
    * refactor: clear polygons in a more robust manner
    
    * chore(docs): add docs and typing to depth related functions
    
    * feat: make depth value spin selector responsive
    
    * refactor: refactor layout for buttons and controls related to 3D mask editing
    
    * remove unused file
    
    * Update environment.yml
    
    * Update setup.py
    
    * chore: improve documention and typing
    
    * feat: update editting when edit mode changes
    
    * reverting pre-commit ruff to only look at import ordering
    
    * fix pre-commit args
    
    ---------
    
    Co-authored-by: henriquenunez <hhlinu@utu.fi>

---
+++ invesalius/data/polygon_select.py
@@ -0,0 +1,85 @@
+# --------------------------------------------------------------------------
+# Software:     InVesalius - Software de Reconstrucao 3D de Imagens Medicas
+# Copyright:    (C) 2001  Centro de Pesquisas Renato Archer
+# Homepage:     http://www.softwarepublico.gov.br
+# Contact:      invesalius@cti.gov.br
+# License:      GNU - GPL 2 (LICENSE.txt/LICENCA.txt)
+# --------------------------------------------------------------------------
+#    Este programa e software livre; voce pode redistribui-lo e/ou
+#    modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
+#    publicada pela Free Software Foundation; de acordo com a versao 2
+#    da Licenca.
+#
+#    Este programa eh distribuido na expectativa de ser util, mas SEM
+#    QUALQUER GARANTIA; sem mesmo a garantia implicita de
+#    COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
+#    PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
+#    detalhes.
+# --------------------------------------------------------------------------
+
+
+from typing import TYPE_CHECKING
+
+import invesalius.constants as const
+from invesalius.gui.widgets.canvas_renderer import CanvasHandlerBase, Polygon
+from invesalius.pubsub import pub as Publisher
+
+if TYPE_CHECKING:
+    import wx
+
+    from invesalius.gui.widgets.canvas_renderer import CanvasEvent, CanvasRendererCTX
+
+
+class PolygonSelectCanvas(CanvasHandlerBase):
+    """Tool for selecting a polygon on a wx-based canvas.
+
+    Inspired on PolygonDensityMeasure, stores and renders a polygon for a canvas.
+
+    Args:
+        colour (tuple[int, int, int, int]): RGBA tuple for the polygon line color.
+        interactive (bool): Whether the polygon is interactive.
+    """
+
+    def __init__(
+        self, colour: tuple[int, int, int, int] = (255, 0, 0, 255), interactive: bool = True
+    ):
+        super().__init__(None)
+
+        self.location = const.SURFACE
+        self.index = 0
+
+        self.polygon = Polygon(self, fill=False, closed=False, line_colour=colour, is_3d=False)
+        self.polygon.layer = 1
+        self.add_child(self.polygon)
+        self.complete = False
+
+        self.interactive = interactive
+
+    def draw_to_canvas(self, gc: "wx.GraphicsContext", canvas: "CanvasRendererCTX") -> None:
+        ## abstract method from super class. needs implementation but does nothing here
+        super().draw_to_canvas(gc, canvas)
+
+    def on_mouse_move(self, _evt: "CanvasEvent"):
+        pass
+
+    def on_drag_end(self, _evt):
+        Publisher.sendMessage("M3E cut mask from 3D")
+
+    def insert_point(self, point: tuple[int, int]):
+        """Insert a new point to the polygon."""
+        self.polygon.append_point(point)
+
+    def complete_polygon(self):
+        if len(self.polygon.points) >= 3:
+            self.polygon.closed = True
+            self.complete = True
+
+    def IsComplete(self):
+        return self.complete
+
+    def SetVisibility(self, value: bool):
+        self.polygon.visible = value
+
+    def set_interactive(self, value: bool):
+        self.interactive = bool(value)
+        self.polygon.interactive = self.interactive
---
+++ invesalius_cy/mask_cut.pyx
@@ -0,0 +1,115 @@
+cimport numpy as np
+cimport cython
+
+from libc.math cimport sqrt
+from cython.parallel import prange
+
+ctypedef np.float64_t DTYPEF64_t
+
+from .cy_my_types cimport image_t, mask_t
+
+@cython.boundscheck(False) # turn of bounds-checking for entire function
+@cython.cdivision(True)
+def mask_cut(np.ndarray[image_t, ndim=3] image,
+                        np.ndarray[int, ndim=1] x_coords,
+                        np.ndarray[int, ndim=1] y_coords,
+                        np.ndarray[int, ndim=1] z_coords,
+                        float sx, float sy, float sz,
+                        float max_depth,
+                        np.ndarray[mask_t, ndim=2] mask,
+                        np.ndarray[DTYPEF64_t, ndim=2] M,
+                        np.ndarray[DTYPEF64_t, ndim=2] MV,
+                        np.ndarray[image_t, ndim=3] out):
+    """
+    Applies a mask cut operation with depth constraint on a 3D image array.
+
+    Parameters
+    ----------
+    image : np.ndarray[image_t, ndim=3]
+        3D array representing the image volume.
+    x_coords : np.ndarray[int, ndim=1]
+        Array of x coordinates where mask is applied.
+    y_coords : np.ndarray[int, ndim=1]
+        Array of y coordinates where mask is applied.
+    z_coords : np.ndarray[int, ndim=1]
+        Array of z coordinates where mask is applied.
+    sx : float
+        Spacing for x axis.
+    sy : float
+        Spacing for y axis.
+    sz : float
+        Scaling factor for z axis.
+    max_depth : float
+        Maximum allowed depth for masking.
+    mask : np.ndarray[mask_t, ndim=2]
+        2D mask to apply in screen space.
+    M : np.ndarray[DTYPEF64_t, ndim=2]
+        4x4 transformation matrix (world to screen).
+    MV : np.ndarray[DTYPEF64_t, ndim=2]
+        4x4 transformation matrix for depth calculation.
+    out : np.ndarray[image_t, ndim=3]
+        Output array to store the result.
+
+    Returns
+    -------
+    None
+        The result is written directly to the `out` array.
+    """
+
+    cdef Py_ssize_t i, N = z_coords.shape[0]
+
+    cdef int x, y, z
+
+    cdef int h = mask.shape[0]
+    cdef int w = mask.shape[1]
+
+    cdef DTYPEF64_t px
+    cdef DTYPEF64_t py
+
+    cdef DTYPEF64_t p0, p1, p2, p3
+    cdef DTYPEF64_t _q0, _q1, _q2, _q3
+    cdef DTYPEF64_t q0, q1, q2
+
+    cdef DTYPEF64_t _c0, _c1, _c2, _c3
+    cdef DTYPEF64_t c0, c1, c2
+
+    cdef DTYPEF64_t dist
+
+    with nogil:
+        for i in prange(N):
+            z = z_coords[i]
+            y = y_coords[i]
+            x = x_coords[i]
+            p0 = <float>(x*sx)
+            p1 = <float>(y*sy)
+            p2 = <float>(z*sz)
+            p3 = 1.0
+
+            _q0 = p0 * M[0, 0] + p1 * M[0, 1] + p2 * M[0, 2] + p3 * M[0, 3]
+            _q1 = p0 * M[1, 0] + p1 * M[1, 1] + p2 * M[1, 2] + p3 * M[1, 3]
+            _q2 = p0 * M[2, 0] + p1 * M[2, 1] + p2 * M[2, 2] + p3 * M[2, 3]
+            _q3 = p0 * M[3, 0] + p1 * M[3, 1] + p2 * M[3, 2] + p3 * M[3, 3]
+
+            if _q3 > 0:
+                q0 = _q0/_q3
+                q1 = _q1/_q3
+                q2 = _q2/_q3
+
+                _c0 = p0 * MV[0, 0] + p1 * MV[0, 1] + p2 * MV[0, 2] + p3 * MV[0, 3]
+                _c1 = p0 * MV[1, 0] + p1 * MV[1, 1] + p2 * MV[1, 2] + p3 * MV[1, 3]
+                _c2 = p0 * MV[2, 0] + p1 * MV[2, 1] + p2 * MV[2, 2] + p3 * MV[2, 3]
+                _c3 = p0 * MV[3, 0] + p1 * MV[3, 1] + p2 * MV[3, 2] + p3 * MV[3, 3]
+
+                c0 = _c0/_c3
+                c1 = _c1/_c3
+                c2 = _c2/_c3
+
+                dist = sqrt(c0*c0 + c1*c1 + c2*c2)
+
+                if dist <= max_depth:
+                    px = (q0/2.0 + 0.5) * (w - 1)
+                    py = (q1/2.0 + 0.5) * (h - 1)
+
+                    if 0 <= px <= w and 0 <= py <= h:
+                        if mask[<int>(py), <int>(px)]:
+                            out[z, y, x] = 0
