b39a6dba6f0e67091473f5ca73620c82fb85ae95
Shivam Chaudhary <sc31122004@gmail.com>
2026-03-06 23:19:53 +0530

    fix(mask): clear 3d polygons when leaving edit in 3d mode (#1185)

--- invesalius/data/styles_3d.py
+++ invesalius/data/styles_3d.py
@@ -777,9 +777,17 @@ class Mask3DEditorInteractorStyle(DefaultInteractorStyle):
         super().CleanUp()
         self.viewer.canvas.unsubscribe_event("LeftButtonPressEvent", self.OnInsertPolygonPoint)
         self.viewer.canvas.unsubscribe_event("LeftButtonDoubleClickEvent", self.OnInsertPolygon)
-        for drawn_polygon in self.m3e_list:
-            drawn_polygon.visible = False
-            drawn_polygon.set_interactive(False)
+
+        # Issue #1078: When the 3D editor is disabled, polygons shouldn't just be hidden
+        # (which doesn't work properly due to CanvasHandlerBase), they should be fully removed.
+        # However, we cannot call self.ClearPolygons() because it also triggers
+        # self.OnRestoreInitMask() which reverts the cut! Instead, just remove the canvases:
+        self.viewer.canvas.draw_list = [
+            drawn_item
+            for drawn_item in self.viewer.canvas.draw_list
+            if not isinstance(drawn_item, PolygonSelectCanvas)
+        ]
+        self.m3e_list.clear()
 
         if self.has_set_mask_preview:
             Publisher.sendMessage("Disable mask 3D preview")
