bf3a811a5032baae0c16baa750c9f66d2ea57488
Prateek Rai <prateekrai903@gmail.com>
2026-03-22 09:22:01 +0530

    fix(nifti): fix mask alignment in export_project_to_nifti - use uint8 label map and identity affine

--- invesalius/project.py
+++ invesalius/project.py
@@ -477,7 +477,7 @@ class Project(metaclass=Singleton):
                 mask_data = mask.matrix[1:, 1:, 1:]
                 # Normalize mask data to uint8 with values {0, 255}
                 mask_data = (mask_data > 127).astype('uint8') * 255
-                mask_nifti = nib.Nifti1Image(np.swapaxes(np.fliplr(mask_data), 0, 2), None)
+                mask_nifti = nib.Nifti1Image(np.swapaxes(np.fliplr(mask_data), 0, 2), np.eye(4))
                 mask_nifti.header.set_zooms(s.spacing)
                 if filename.lower().endswith(".nii"):
                     basename = filename[:-4]
