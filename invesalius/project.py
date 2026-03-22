6e569b84d277e3d5464d1acf027ab1a140f017a5
Prateek Rai <prateekrai903@gmail.com>
2026-03-22 08:56:35 +0530

    fix(nifti): export masks as uint8 label maps {0,255} in export_project_to_nifti

--- invesalius/project.py
+++ invesalius/project.py
@@ -475,6 +475,8 @@ class Project(metaclass=Singleton):
                 s.do_threshold_to_all_slices(mask)
 
                 mask_data = mask.matrix[1:, 1:, 1:]
+                # Normalize mask data to uint8 with values {0, 255}
+                mask_data = (mask_data > 127).astype('uint8') * 255
                 mask_nifti = nib.Nifti1Image(np.swapaxes(np.fliplr(mask_data), 0, 2), None)
                 mask_nifti.header.set_zooms(s.spacing)
                 if filename.lower().endswith(".nii"):
